773d2bc81f430b1778790ec58fbcf3e7fab4a7fa
seyoung5744 <seyoung574458@gmail.com>
2023-09-28 20:47:22 +0900

    init Firebase Cloud Messaging project

---
+++ Spring/springboot-fcm/src/main/java/com/example/springbootfcm/SpringbootFcmApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootfcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFcmApplication.class, args);
+	}
+
+}
