35bfecf9f928c317e2c9ba2c0653d5e37c29d5d4
seyoung5744 <seyoung574458@gmail.com>
2024-01-12 15:41:04 +0900

    Redisson 을 활용하여 재고로직 작성하기

---
+++ Spring/stock-concurrency/src/main/java/com/example/stock/facade/RedissonLockStockFacade.java
@@ -0,0 +1,43 @@
+package com.example.stock.facade;
+
+import com.example.stock.service.StockService;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class RedissonLockStockFacade {
+
+    private RedissonClient redissonClient;
+
+    private StockService stockService;
+
+    public RedissonLockStockFacade(RedissonClient redissonClient, StockService stockService) {
+        this.redissonClient = redissonClient;
+        this.stockService = stockService;
+    }
+
+    public void decrease(Long id, Long quantity) {
+        RLock lock = redissonClient.getLock(id.toString());
+
+        try {
+            boolean available = lock.tryLock(10, 1, TimeUnit.SECONDS);
+
+            if (!available) {
+                log.info("lock 획득 실패");
+                return;
+            }
+
+            stockService.decrease(id, quantity);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(lock != null && lock.isLocked()) {
+                lock.unlock();
+            }
+        }
+    }
+}
