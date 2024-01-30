c71edab08c784192109e9be3dd8835a421f0c026
seyoung5744 <seyoung574458@gmail.com>
2024-01-30 16:43:09 +0900

    소수, 비소수 판별 비동기 테스트로 변경

--- JAVA/Modern_Java_in_Action/src/main/java/chap06/collector/CollectorHarness.java
+++ JAVA/Modern_Java_in_Action/src/main/java/chap06/collector/CollectorHarness.java
@@ -1,16 +1,41 @@
 package chap06.collector;
 
 import chap03.FunctionalInterfaceTest.Consumer;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CollectorHarness {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // ExecutorService 생성
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        // CompletableFuture로 각 메소드를 비동기적으로 실행
+        CompletableFuture<Long> future1 = CompletableFuture.supplyAsync(() -> execute(PartitionPrimeNumbers::partitionPrimes), executorService);
+        CompletableFuture<Long> future2 = CompletableFuture.supplyAsync(() -> execute(PartitionPrimeNumbers::partitionPrimesWithCustomCollector), executorService);
+        CompletableFuture<Long> future3 = CompletableFuture.supplyAsync(() -> execute(PartitionPrimeNumbers::partitionPrimesWithInlineCollector), executorService);
+
+        // 모든 작업이 완료될 때까지 대기
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(future1, future2, future3);
+        allOf.join();
+
+        // 결과 출력
+        System.out.println("Partitioning done in: " + future1.get() + " msecs");
+        System.out.println("Partitioning done in: " + future2.get() + " msecs");
+        System.out.println("Partitioning done in: " + future3.get() + " msecs");
+
+        // ExecutorService 종료
+        executorService.shutdown();
+
 //        System.out.println("Partitioning done in: " + execute(PartitionPrimeNumbers::partitionPrimes) + " msecs");
 //        System.out.println("Partitioning done in: " + execute(PartitionPrimeNumbers::partitionPrimesWithCustomCollector) + " msecs");
-        System.out.println("Partitioning done in: " + execute(PartitionPrimeNumbers::partitionPrimesWithInlineCollector) + " msecs");
+//        System.out.println("Partitioning done in: " + execute(PartitionPrimeNumbers::partitionPrimesWithInlineCollector) + " msecs");
     }
 
-    public static long execute(Consumer<Integer> primePartitioner) {
+
+    public static synchronized long execute(Consumer<Integer> primePartitioner) {
         long fastest = Long.MAX_VALUE;
 
         for (int i = 0; i < 10; i++) { // 테스트 10번 반복
