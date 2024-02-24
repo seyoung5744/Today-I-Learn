bf2dcba1186f42572317c0d13a9ea94251b51927
seyoung5744 <seyoung574458@gmail.com>
2024-02-24 15:23:57 +0900

    13.4 해석 규칙

---
+++ JAVA/Modern_Java_in_Action/src/main/java/chap13/_13_4/Ambiguous.java
@@ -0,0 +1,32 @@
+package chap13._13_4;
+
+public class Ambiguous {
+    public static void main(String... args) {
+        new C().hello();
+    }
+
+    static interface A {
+
+        public default void hello() {
+            System.out.println("Hello from A");
+        }
+
+    }
+
+    static interface B {
+
+        public default void hello() {
+            System.out.println("Hello from B");
+        }
+
+    }
+
+    static class C implements B, A {
+
+        @Override
+        public void hello() {
+            A.super.hello();
+        }
+
+    }
+}
