06574780f8f08246b2a5303fba0646635009941f
seyoung5744 <seyoung574458@gmail.com>
2024-03-03 22:59:33 +0900

    스프링 트랜잭션 이해

---
+++ Spring/spring-db/springtx/src/main/java/com/example/springtx/order/Order.java
@@ -0,0 +1,24 @@
+package com.example.springtx.order;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username; // 정상, 예외, 잔고 부족
+    private String payStatus; // 대기, 완료
+
+}
