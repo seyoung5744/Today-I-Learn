2b6ee5d3e3ddd755935d30c0ae0e3dbdd43200e9
seyoung5744 <seyoung574458@gmail.com>
2024-04-08 16:34:25 +0900

    11장. 모든 악의 근원

--- JAVA/TDD/Currency/src/test/java/MoneyTest.java
+++ JAVA/TDD/Currency/src/test/java/MoneyTest.java
@@ -16,29 +16,13 @@ public class MoneyTest {
     public void equality() {
         assertThat(Money.dollar(5)).isEqualTo(Money.dollar(5));
         assertThat(Money.dollar(5)).isNotEqualTo(Money.dollar(6));
-        assertThat(Money.franc(5)).isEqualTo(Money.franc(5));
-        assertThat(Money.franc(5)).isNotEqualTo(Money.franc(6));
         assertThat(Money.franc(5)).isNotEqualTo(Money.dollar(5));
-    }
-
-    @Test
-    public void francMultiplicationTest() {
-        Money five = Money.franc(5);
-
-        assertThat(five.times(2)).isEqualTo(Money.franc(10));
-        assertThat(five.times(3)).isEqualTo(Money.franc(15));
     }
 
     @Test
     public void currencyTest() {
         assertThat(Money.dollar(1).currency()).isEqualTo("USD");
         assertThat(Money.franc(1).currency()).isEqualTo("CHF");
-    }
-
-    @Test
-    public void differentClassEquality() {
-        assertThat(new Franc(10, "CHF")).isEqualTo(new Money(10, "CHF"));
-        assertThat(new Dollar(10, "USD")).isEqualTo(new Money(10, "USD"));
     }
 }
 
@@ -61,11 +45,11 @@ class Money {
     }
 
     static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     @Override
@@ -80,18 +64,3 @@ class Money {
         return amount + " " + currency;
     }
 }
-
-class Dollar extends Money {
-
-    Dollar(int amount, String currency) {
-        super(amount, currency);
-    }
-}
-
-class Franc extends Money {
-
-    Franc(int amount, String currency) {
-        super(amount, currency);
-    }
-}
-
