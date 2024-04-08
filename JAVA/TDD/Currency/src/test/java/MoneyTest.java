bb46dabaf98e391b26cea48c8c037c22205d2838
seyoung5744 <seyoung574458@gmail.com>
2024-04-08 14:51:15 +0900

    8장. 객체 만들기

--- JAVA/TDD/Currency/src/test/java/MoneyTest.java
+++ JAVA/TDD/Currency/src/test/java/MoneyTest.java
@@ -6,33 +6,43 @@ public class MoneyTest {
 
     @Test
     public void multiplicationTest() {
-        Dollar five = new Dollar(5);
+        Money five = Money.dollar(5);
 
-        assertThat(new Dollar(10)).isEqualTo(five.times(2));
-        assertThat(new Dollar(15)).isEqualTo(five.times(3));
+        assertThat(Money.dollar(10)).isEqualTo(five.times(2));
+        assertThat(Money.dollar(15)).isEqualTo(five.times(3));
     }
 
     @Test
     public void equality() {
-        assertThat(new Dollar(5)).isEqualTo(new Dollar(5));
-        assertThat(new Dollar(5)).isNotEqualTo(new Dollar(6));
-        assertThat(new Franc(5)).isEqualTo(new Franc(5));
-        assertThat(new Franc(5)).isNotEqualTo(new Franc(6));
-        assertThat(new Franc(5)).isNotEqualTo(new Dollar(5));
+        assertThat(Money.dollar(5)).isEqualTo(Money.dollar(5));
+        assertThat(Money.dollar(5)).isNotEqualTo(Money.dollar(6));
+        assertThat(Money.franc(5)).isEqualTo(Money.franc(5));
+        assertThat(Money.franc(5)).isNotEqualTo(Money.franc(6));
+        assertThat(Money.franc(5)).isNotEqualTo(Money.dollar(5));
     }
 
     @Test
     public void francMultiplicationTest() {
-        Franc five = new Franc(5);
+        Money five = Money.franc(5);
 
-        assertThat(new Franc(10)).isEqualTo(five.times(2));
-        assertThat(new Franc(15)).isEqualTo(five.times(3));
+        assertThat(Money.franc(10)).isEqualTo(five.times(2));
+        assertThat(Money.franc(15)).isEqualTo(five.times(3));
     }
 }
 
-class Money {
+abstract class Money {
 
     protected int amount;
+
+    abstract Money times(int multiplier);
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     @Override
     public boolean equals(Object obj) {
@@ -48,7 +58,7 @@ class Dollar extends Money {
         this.amount = amount;
     }
 
-    Dollar times(int multiplier) {
+    Money times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
 }
@@ -59,7 +69,7 @@ class Franc extends Money {
         this.amount = amount;
     }
 
-    Franc times(int multiplier) {
+    Money times(int multiplier) {
         return new Franc(amount * multiplier);
     }
 }
