857f08bc58ae890baedfc8be0072d73c0248a07f
seyoung5744 <seyoung574458@gmail.com>
2024-04-13 16:17:24 +0900

    Assert 만들기

--- JAVA/TDD/my-junit/src/main/java/TestCaseTest.java
+++ JAVA/TDD/my-junit/src/main/java/TestCaseTest.java
@@ -7,9 +7,9 @@ public class TestCaseTest extends TestCase {
     // 하나의 test 해야할 코드라고 생각하자.
     public void testRunning() {
         WasRun test = new WasRun("testMethod"); // 실행할 테스트 메서드의 이름 전달
-        System.out.println("test.wasRun() = " + test.wasRun);
+        Assert.assertEquals(test.wasRun, false);
         test.run();
-        System.out.println("test.wasRun() = " + test.wasRun);
+        Assert.assertEquals(test.wasRun, true);
     }
 
 }
