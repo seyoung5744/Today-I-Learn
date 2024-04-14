aded412d770ef5eae8c128783e25e20ea889eed6
seyoung5744 <seyoung574458@gmail.com>
2024-04-14 22:50:37 +0900

    로그 추적기 V1 - 적용

---
+++ Spring/spring-advanced/advanced/src/main/java/com/example/advanced/aop/v2/OrderControllerV2.java
@@ -0,0 +1,31 @@
+package com.example.advanced.aop.v2;
+
+import com.example.advanced.trace.HelloTraceV2;
+import com.example.advanced.trace.TraceStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/request")
+    public String request(@RequestParam String itemId) {
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
