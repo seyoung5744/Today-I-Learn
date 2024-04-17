44040f11020acb86de34678bf34fdae5d587d84c
seyoung5744 <seyoung574458@gmail.com>
2024-04-17 21:14:52 +0900

    템플릿 메서드 패턴 - 예제1

---
+++ Spring/spring-advanced/advanced/src/test/java/com/example/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,35 @@
+package com.example.advanced.trace.template;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    public void templateMethodV0() {
+        logic1();
+        logic2();
+    }
+
+    public void logic1() {
+        long startTime = System.currentTimeMillis();
+        // 비지니스 로직 실행
+        log.info("비지니즈 로직1 실행");
+        // 비지니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    public void logic2() {
+        long startTime = System.currentTimeMillis();
+        // 비지니스 로직 실행
+        log.info("비지니즈 로직2 실행");
+        // 비지니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+}
