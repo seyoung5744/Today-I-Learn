d833b6ce66a2c07a731b81aca9b620d510669504
seyoung5744 <seyoung574458@gmail.com>
2024-04-17 21:21:15 +0900

    템플릿 메서드 패턴 - 예제2

--- Spring/spring-advanced/advanced/src/test/java/com/example/advanced/trace/template/TemplateMethodTest.java
+++ Spring/spring-advanced/advanced/src/test/java/com/example/advanced/trace/template/TemplateMethodTest.java
@@ -1,5 +1,8 @@
 package com.example.advanced.trace.template;
 
+import com.example.advanced.trace.template.code.AbstractTemplate;
+import com.example.advanced.trace.template.code.SubClassLogic1;
+import com.example.advanced.trace.template.code.SubClassLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +13,18 @@ public class TemplateMethodTest {
     public void templateMethodV0() {
         logic1();
         logic2();
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    public void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
     }
 
     public void logic1() {
