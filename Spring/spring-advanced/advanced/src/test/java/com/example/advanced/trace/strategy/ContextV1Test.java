e212893356955225f76a0a0c6477d50b25660549
seyoung5744 <seyoung574458@gmail.com>
2024-04-17 21:54:44 +0900

    전략 패턴 - 예제2

--- Spring/spring-advanced/advanced/src/test/java/com/example/advanced/trace/strategy/ContextV1Test.java
+++ Spring/spring-advanced/advanced/src/test/java/com/example/advanced/trace/strategy/ContextV1Test.java
@@ -30,6 +30,66 @@ public class ContextV1Test {
         context2.execute();
     }
 
+    /**
+     * 전략 패턴 익명 내부 클래스1
+     */
+    @Test
+    public void strategyV2() {
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        };
+        Context1 context1 = new Context1(strategyLogic1);
+        log.info("strategyLogic1={}", strategyLogic1.getClass());
+        context1.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직2 실행");
+            }
+        };
+        Context1 context2 = new Context1(strategyLogic2);
+        log.info("strategyLogic2={}", strategyLogic2.getClass());
+        context2.execute();
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스2
+     */
+    @Test
+    public void strategyV3() {
+        Context1 context1 = new Context1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        });
+        context1.execute();
+
+        Context1 context2 = new Context1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직2 실행");
+            }
+        });
+        context2.execute();
+    }
+
+    /**
+     * 전략 패턴, 람다
+     */
+    @Test
+    public void strategyV4() {
+        Context1 context1 = new Context1(() -> log.info("비지니스 로직1 실행"));
+        context1.execute();
+
+        Context1 context2 = new Context1(() -> log.info("비지니스 로직2 실행"));
+        context2.execute();
+    }
+
     public void logic1() {
         long startTime = System.currentTimeMillis();
         // 비지니스 로직 실행
