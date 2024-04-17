e755fb8b8857e41499a179bfd2c6c590dbb1a5f8
seyoung5744 <seyoung574458@gmail.com>
2024-04-17 21:48:11 +0900

    전략 패턴 - 예제1

--- Spring/spring-advanced/advanced/src/test/java/com/example/advanced/trace/strategy/ContextV1Test.java
+++ Spring/spring-advanced/advanced/src/test/java/com/example/advanced/trace/strategy/ContextV1Test.java
@@ -1,14 +1,33 @@
 package com.example.advanced.trace.strategy;
 
+import com.example.advanced.trace.strategy.code.strategy.Context1;
+import com.example.advanced.trace.strategy.code.strategy.Strategy;
+import com.example.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import com.example.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class ContextV1Test {
+
     @Test
     public void strategyV0() {
         logic1();
         logic2();
+    }
+
+    /**
+     * 전략 패턴 적용
+     */
+    @Test
+    public void strategyV1() {
+        Strategy strategyLogic1 = new StrategyLogic1();
+        Context1 context1 = new Context1(strategyLogic1);
+        context1.execute();
+
+        Strategy strategyLogic2 = new StrategyLogic2();
+        Context1 context2 = new Context1(strategyLogic2);
+        context2.execute();
     }
 
     public void logic1() {
