094741f454b34f832792d3f81e24eb9d031afec5
seyoung5744 <seyoung574458@gmail.com>
2024-04-19 16:45:04 +0900

    프록시 패턴 - 예제 코드1

---
+++ Spring/spring-advanced/proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,20 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ProxyPatternTest {
+
+    @Test
+    public void noProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
