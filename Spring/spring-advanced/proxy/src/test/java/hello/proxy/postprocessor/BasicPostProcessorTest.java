cbf7adfe1f9bf78ee43ffca5e6560abf024ee075
seyoung5744 <seyoung574458@gmail.com>
2024-04-20 17:06:25 +0900

    빈 후처리기 - 예제 코드2

---
+++ Spring/spring-advanced/proxy/src/test/java/hello/proxy/postprocessor/BasicPostProcessorTest.java
@@ -0,0 +1,88 @@
+package hello.proxy.postprocessor;
+
+import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BasicPostProcessorTest {
+
+    @Test
+    public void basicTest() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanPostProcessorConfig.class);
+
+        // beanA 이름으로 B 객체가 빈으로 등록된다.
+        B b = applicationContext.getBean("beanA", B.class);
+        b.helloB();
+
+        // A 는 빈으로 등록되지 않는다.
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean(A.class));
+    }
+
+    @Slf4j
+    @Configuration
+    static class BeanPostProcessorConfig {
+
+        @Bean(name = "beanA")
+        public A a() {
+            return new A();
+        }
+
+        @Bean
+        public AToBPostProcessor helloPostProcessor() {
+            return new AToBPostProcessor();
+        }
+    }
+
+    @Slf4j
+    static class A {
+
+        public void helloA() {
+            log.info("helloA");
+        }
+
+        @PostConstruct
+        public void init() {
+            log.info("A - PostConstruct");
+        }
+    }
+
+    @Slf4j
+    static class B {
+
+        public void helloB() {
+            log.info("helloB");
+        }
+
+        @PostConstruct
+        public void init() {
+            log.info("B - PostConstruct");
+        }
+    }
+
+    @Slf4j
+    static class AToBPostProcessor implements BeanPostProcessor {
+
+        @Override
+        public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+            log.info("beanName={} bean={}", beanName, bean);
+            if(bean instanceof A) {
+                return new B();
+            }
+            return bean;
+        }
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            log.info("postProcessAfterInitialization 실행");
+            return bean;
+        }
+    }
+}
