1dbf2cf22797582614726f5ab266d72805d7dd62
seyoung5744 <seyoung574458@gmail.com>
2024-04-26 17:13:33 +0900

    뷰 리졸버

--- Spring/spring_MVC/servlet/src/main/java/hello/servlet/web/springmvc/old/OlcController.java
+++ Spring/spring_MVC/servlet/src/main/java/hello/servlet/web/springmvc/old/OlcController.java
@@ -12,6 +12,6 @@ public class OlcController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OlcController.handleRequest");
-        return null;
+        return new ModelAndView("new-form");
     }
 }
