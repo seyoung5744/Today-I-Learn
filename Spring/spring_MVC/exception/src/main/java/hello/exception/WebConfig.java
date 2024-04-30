4678af59d545a5145afd42d4a5600fdc57dca843
seyoung5744 <seyoung574458@gmail.com>
2024-04-30 16:39:58 +0900

    HandlerExceptionResolver 시작

--- Spring/spring_MVC/exception/src/main/java/hello/exception/WebConfig.java
+++ Spring/spring_MVC/exception/src/main/java/hello/exception/WebConfig.java
@@ -2,11 +2,14 @@ package hello.exception;
 
 import hello.exception.filter.LogFilter;
 import hello.exception.interceptor.LogInterceptor;
+import hello.exception.resolver.MyHandlerExceptionResolver;
+import java.util.List;
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,7 +27,12 @@ public class WebConfig implements WebMvcConfigurer {
             );
     }
 
-//    @Bean
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+    }
+
+    //    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
