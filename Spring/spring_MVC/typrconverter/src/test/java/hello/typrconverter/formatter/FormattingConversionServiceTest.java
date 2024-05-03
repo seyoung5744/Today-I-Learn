d444c5de4f4f87bf214ed834ae15dd22b7916194
seyoung5744 <seyoung574458@gmail.com>
2024-05-03 15:23:09 +0900

    포맷터를 지원하는 컨버전 서비스

---
+++ Spring/spring_MVC/typrconverter/src/test/java/hello/typrconverter/formatter/FormattingConversionServiceTest.java
@@ -0,0 +1,32 @@
+package hello.typrconverter.formatter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import hello.typrconverter.converter.IpPortToStringConverter;
+import hello.typrconverter.converter.StringToIpPortConverter;
+import hello.typrconverter.type.IpPort;
+import org.junit.jupiter.api.Test;
+import org.springframework.format.support.DefaultFormattingConversionService;
+
+public class FormattingConversionServiceTest {
+
+    @Test
+    public void formattingConversionService() {
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+
+        // 컨버터 등록
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        // 포맷터 등록
+        conversionService.addFormatter(new MyNumberFormatter());
+
+        // 컨버터 사용
+        IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+
+        // 포맷터 사용
+        assertThat(conversionService.convert(1000, String.class)).isEqualTo("1,000");
+        assertThat(conversionService.convert("1,000", Long.class)).isEqualTo(1000L);
+    }
+}
