74e539a4b38b0ac377cba574d63d88a85f0351f3
seyoung5744 <seyoung574458@gmail.com>
2024-05-03 14:03:46 +0900

    스프링 타입 컨버터 소개

---
+++ Spring/spring_MVC/typrconverter/src/test/java/hello/typrconverter/TyprconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.typrconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyprconverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
