6a0b74caa4b4af5e505dd07b6ffa0602d56813a9
seyoung5744 <seyoung574458@gmail.com>
2024-05-03 15:24:54 +0900

    포맷터 적용하기

--- Spring/spring_MVC/typrconverter/src/main/java/hello/typrconverter/WebConfig.java
+++ Spring/spring_MVC/typrconverter/src/main/java/hello/typrconverter/WebConfig.java
@@ -1,9 +1,8 @@
 package hello.typrconverter;
 
-import hello.typrconverter.converter.IntegerToStringConverter;
 import hello.typrconverter.converter.IpPortToStringConverter;
-import hello.typrconverter.converter.StringToIntegerConverter;
 import hello.typrconverter.converter.StringToIpPortConverter;
+import hello.typrconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        // 주석처리 => 우선 순위
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        // 추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
