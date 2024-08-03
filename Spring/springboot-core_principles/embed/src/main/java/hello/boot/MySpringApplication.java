eb355498d7bdef20fafcb4095c5b8b6485040d49
seyoung5744 <seyoung574458@gmail.com>
2024-08-03 15:26:12 +0900

    편리한 부트 클래스 만들기

--- Spring/springboot-core_principles/embed/src/main/java/hello/boot/MySpringApplication.java
+++ Spring/springboot-core_principles/embed/src/main/java/hello/boot/MySpringApplication.java
@@ -1,7 +1,6 @@
-package hello.embed;
+package hello.boot;
 
-import hello.spring.HelloConfig;
-import java.io.File;
+import java.util.List;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.connector.Connector;
@@ -9,10 +8,10 @@ import org.apache.catalina.startup.Tomcat;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-public class EmbedTomcatSpringMain {
+public class MySpringApplication {
 
-    public static void main(String[] args) throws LifecycleException {
-        System.out.println("EmbedTomcatSpringMain.main");
+    public static void run(Class configClass, String[] args) {
+        System.out.println("MySpringApplication.main args=" + List.of(args));
 
         // 톰캣 설정
         Tomcat tomcat = new Tomcat();
@@ -22,25 +21,20 @@ public class EmbedTomcatSpringMain {
 
         // 스프링 컨테이너 생성
         AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
-        appContext.register(HelloConfig.class);
+        appContext.register(configClass);
 
         // 스프링 MVC 디스패처 서블릿 생성, 스프링 컨테이너 연결
         DispatcherServlet dispatcher = new DispatcherServlet(appContext);
 
-        // 서블릿 등록
+        // 디스패처 서블릿 등록
         Context context = tomcat.addContext("", "/");
-
-        //== 코드 추가 시작==
-        File docBaseFile = new File(context.getDocBase());
-        if (!docBaseFile.isAbsolute()) {
-            docBaseFile = new File(((org.apache.catalina.Host) context.getParent()).getAppBaseFile(), docBaseFile.getPath());
-        }
-        docBaseFile.mkdirs();
-        //== 코드 추가 종료==
-
         tomcat.addServlet("", "dispatcher", dispatcher);
         context.addServletMappingDecoded("/", "dispatcher");
 
-        tomcat.start();
+        try {
+            tomcat.start();
+        } catch (LifecycleException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
