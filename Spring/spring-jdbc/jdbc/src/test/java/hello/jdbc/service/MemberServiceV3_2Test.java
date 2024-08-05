975e68d39a1b81aa17e6983637dbfa18e7e1e101
seyoung5744 <seyoung574458@gmail.com>
2024-08-05 22:33:29 +0900

    트랜잭션 문제 해결 - 트랜잭션 템플릿

---
+++ Spring/spring-jdbc/jdbc/src/test/java/hello/jdbc/service/MemberServiceV3_2Test.java
@@ -0,0 +1,96 @@
+package hello.jdbc.service;
+
+import static hello.jdbc.connection.ConnectionConst.PASSWORD;
+import static hello.jdbc.connection.ConnectionConst.URL;
+import static hello.jdbc.connection.ConnectionConst.USERNAME;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.zaxxer.hikari.HikariDataSource;
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import java.sql.SQLException;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+/**
+ * 트랜잭션 - 트랜잭션 템플릿
+ */
+@Slf4j
+class MemberServiceV3_2Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV3 memberRepository;
+    private MemberServiceV3_2 memberService;
+
+    @BeforeEach
+    void before() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        memberRepository = new MemberRepositoryV3(dataSource);
+
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
+        memberService = new MemberServiceV3_2(transactionManager, memberRepository);
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        log.info("=========delete==================");
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    public void accountTransfer() throws SQLException {
+        //given
+        Member memberA = new Member(MEMBER_A, 10_000);
+        Member memberB = new Member(MEMBER_B, 10_000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        //when
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2_000);
+
+        //then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberB.getMemberId());
+        assertThat(findMemberA.getMoney()).isEqualTo(8_000);
+        assertThat(findMemberB.getMoney()).isEqualTo(12_000);
+    }
+
+    @Test
+    @DisplayName("이체 중 예외 발생")
+    public void accountTransferEx() throws SQLException {
+        //given
+        Member memberA = new Member(MEMBER_A, 10_000);
+        Member memberEx = new Member(MEMBER_EX, 10_000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+
+        //when
+        assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2_000))
+            .isInstanceOf(IllegalStateException.class);
+
+        //then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberEx = memberRepository.findById(memberEx.getMemberId());
+
+        // memberA의 돈이 롤백 되어야함
+        assertThat(findMemberA.getMoney()).isEqualTo(10_000);
+        assertThat(findMemberEx.getMoney()).isEqualTo(10_000);
+    }
+
+}
