5350062650fb6f61dd93aeb5027a9a239ab4b26b
seyoung5744 <seyoung574458@gmail.com>
2024-08-17 20:20:15 +0900

    Persistence Layer 테스트 (2)

--- Spring/practical-testing/cafekiosk/src/main/java/sample/cafekiosk/spring/domain/product/Product.java
+++ Spring/practical-testing/cafekiosk/src/main/java/sample/cafekiosk/spring/domain/product/Product.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sample.cafekiosk.spring.domain.BaseEntity;
@@ -32,4 +33,12 @@ public class Product extends BaseEntity {
 
     private int price;
 
+    @Builder
+    public Product(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
 }
