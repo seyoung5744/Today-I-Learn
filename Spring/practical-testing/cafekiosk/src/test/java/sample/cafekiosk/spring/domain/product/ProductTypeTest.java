11bc06175988a5644f4fc9a90ce3365781c7a731
seyoung5744 <seyoung574458@gmail.com>
2024-08-23 14:44:47 +0900

    한 문단에 한 주제!

--- Spring/practical-testing/cafekiosk/src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ Spring/practical-testing/cafekiosk/src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -32,4 +32,30 @@ class ProductTypeTest {
         //then
         assertThat(result).isTrue();
     }
+
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @Test
+    void containsStockTypeEx() {
+        //given
+        ProductType[] productTypes = ProductType.values();
+
+        for (ProductType productType : productTypes) {
+            if (productType == ProductType.HANDMADE) {
+                //when
+                boolean result = ProductType.containsStockType(productType);
+
+                //then
+                assertThat(result).isFalse();
+            }
+
+            if (productType == ProductType.BAKERY || productType == ProductType.BOTTLE) {
+                //when
+                boolean result = ProductType.containsStockType(productType);
+
+                //then
+                assertThat(result).isTrue();
+            }
+        }
+
+    }
 }
