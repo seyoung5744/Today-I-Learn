66de7b337fd140a3d86c4e013d2cdd35e56f5ea0
seyoung5744 <seyoung574458@gmail.com>
2024-08-30 21:55:44 +0900

    API 테스트로 전환하기

--- Spring/practical-testing/product-order-service/src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ Spring/practical-testing/product-order-service/src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -3,6 +3,9 @@ package com.example.productorderservice.product;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.productorderservice.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -20,4 +23,20 @@ public class ProductApiTest extends ApiTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
+    @DisplayName("상품 조회")
+    @Test
+    void getProduct() {
+        final var request = ProductSteps.createAddProductRequest();
+        ProductSteps.requestAddProduct(request);
+        Long productId = 1L;
+
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .when()
+            .get("/products/{productId}", productId)
+            .then().log().all()
+            .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
+    }
 }
