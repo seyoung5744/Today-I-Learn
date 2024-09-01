053449116acb2679c03cb74c8ba4c55d1d5cdbef
seyoung5744 <seyoung574458@gmail.com>
2024-09-01 16:58:55 +0900

    API 테스트로 전환하기

--- Spring/practical-testing/product-order-service/src/test/java/com/example/productorderservice/payment/PaymentSteps.java
+++ Spring/practical-testing/product-order-service/src/test/java/com/example/productorderservice/payment/PaymentSteps.java
@@ -1,6 +1,10 @@
 package com.example.productorderservice.payment;
 
 import com.example.productorderservice.payment.PaymentRequest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
 
 public class PaymentSteps {
 
@@ -8,5 +12,15 @@ public class PaymentSteps {
         final Long orderId = 1L;
         final String cardNumber = "1234-1234-1234-1234";
         return new PaymentRequest(orderId, cardNumber);
+    }
+
+    static ExtractableResponse<Response> paymentRequest(PaymentRequest request) {
+        return RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(request)
+            .when()
+            .post("/payments")
+            .then()
+            .log().all().extract();
     }
 }
