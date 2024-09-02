7c907de8da8ef82e1bff41b58a1e9bb754fce8ef
seyoung5744 <seyoung574458@gmail.com>
2024-09-02 14:29:25 +0900

    테스트하기 어려운 영역을 분리하기

--- Spring/spring-test/cafekiosk/src/main/java/sample/cafekiosk/unit/CafeKioskRunner.java
+++ Spring/spring-test/cafekiosk/src/main/java/sample/cafekiosk/unit/CafeKioskRunner.java
@@ -1,7 +1,9 @@
 package sample.cafekiosk.unit;
 
+import java.time.LocalDateTime;
 import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
+import sample.cafekiosk.unit.order.Order;
 
 public class CafeKioskRunner {
 
@@ -15,5 +17,7 @@ public class CafeKioskRunner {
 
         int totalPrice = cafeKiosk.calculateTotalPrice();
         System.out.println("총 주문 가격 : " + totalPrice);
+
+        Order order = cafeKiosk.createOrder(LocalDateTime.now());
     }
 }
