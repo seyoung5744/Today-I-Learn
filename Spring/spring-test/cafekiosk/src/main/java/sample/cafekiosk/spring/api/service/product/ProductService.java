7f18bc8a5cc642121a6146cbd4477caeff15c4dd
seyoung5744 <seyoung574458@gmail.com>
2024-09-06 17:31:38 +0900

    Presentation Layer 테스트 (1)

--- Spring/spring-test/cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ Spring/spring-test/cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -4,16 +4,36 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 
-@Service
+/**
+ * readOnly = true : 읽기 전용
+ * </br> CRUD 에서 CUD 동작 X / only Read
+ * </br> JPA : CUD 스냅샷 저장, 변경감지 X (성능 향상)
+ * <p>
+ * CQRS - Command / Query
+ */
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Service
 public class ProductService {
 
     private final ProductRepository productRepository;
+
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
 
     public List<ProductResponse> getSellingProducts() {
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
@@ -21,5 +41,15 @@ public class ProductService {
         return products.stream()
             .map(ProductResponse::of)
             .collect(Collectors.toList());
+    }
+
+    private String createNextProductNumber() {
+        return productRepository.findLatestProductNumber()
+            .map(latestProductNumber -> {
+                    int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+                    int nextProductNumberInt = latestProductNumberInt + 1;
+                    return String.format("%03d", nextProductNumberInt);
+                }
+            ).orElse("001");
     }
 }
