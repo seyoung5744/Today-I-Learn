30bfbd1a59898d20c97bcc2bf219c2fb487d697a
seyoung5744 <seyoung574458@gmail.com>
2024-09-13 15:40:27 +0900

    JPA 방식으로 데이터 저장하기

--- Spring/mini-project/weather/src/main/java/zerobase/weather/domain/Memo.java
+++ Spring/mini-project/weather/src/main/java/zerobase/weather/domain/Memo.java
@@ -1,5 +1,9 @@
 package zerobase.weather.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,8 +12,11 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class Memo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String text;
 
