70d981b52d973bf4d22a089c90c5bce22ec0cb2b
seyoung5744 <seyoung574458@gmail.com>
2024-09-20 17:13:58 +0900

    회원관리 - 회사 삭제 기능 구현

--- Spring/mini-project/Dividend/src/main/java/zerobase/dividend/persist/DividendRepository.java
+++ Spring/mini-project/Dividend/src/main/java/zerobase/dividend/persist/DividendRepository.java
@@ -4,12 +4,16 @@ import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import zerobase.dividend.persist.domain.DividendEntity;
 
 @Repository
 public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
 
     List<DividendEntity> findAllByCompanyId(Long companyId);
+
+    @Transactional
+    void deleteAllByCompanyId(Long id);
 
     boolean existsByCompanyIdAndDate(Long companyId, LocalDateTime date);
 }
--- Spring/mini-project/Dividend/src/main/java/zerobase/dividend/web/CompanyController.java
+++ Spring/mini-project/Dividend/src/main/java/zerobase/dividend/web/CompanyController.java
@@ -1,7 +1,7 @@
 package zerobase.dividend.web;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -9,12 +9,14 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import zerobase.dividend.model.Company;
+import zerobase.dividend.model.constants.CacheKey;
 import zerobase.dividend.persist.domain.CompanyEntity;
 import zerobase.dividend.service.CompanyService;
 
@@ -24,6 +26,7 @@ import zerobase.dividend.service.CompanyService;
 public class CompanyController {
 
     private final CompanyService companyService;
+    private final CacheManager cacheManager;
 
     @GetMapping("/autocomplete")
     public ResponseEntity<?> autocomplete(@RequestParam("keyword") String keyword) {
@@ -49,8 +52,15 @@ public class CompanyController {
         return ResponseEntity.ok(company);
     }
 
-    @DeleteMapping
-    public ResponseEntity<?> deleteCompany() {
-        return null;
+    @DeleteMapping("/{ticker}")
+    @PreAuthorize("hasRole('WRITE')")
+    public ResponseEntity<?> deleteCompany(@PathVariable String ticker) {
+        String companyName = companyService.deleteCompany(ticker);
+        clearFinanceCache(companyName);
+        return ResponseEntity.ok(companyName);
+    }
+
+    private void clearFinanceCache(final String companyName) {
+        cacheManager.getCache(CacheKey.KEY_FINANCE).evict(companyName);
     }
 }
