40a1d6e56127c3bc8d2d0dafa4cb8d1a86b26244
seyoung5744 <seyoung574458@gmail.com>
2024-10-20 17:40:00 +0900

    create multi module test project

---
+++ Spring/springboot-multi-module/src/test/java/com/example/springbootmultimodule/SpringbootMultiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmultimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMultiModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
