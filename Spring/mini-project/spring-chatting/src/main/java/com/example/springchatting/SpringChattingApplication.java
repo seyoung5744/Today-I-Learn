b0016f8341adf731ee4cb8db72d82e27f05fc141
seyoung5744 <seyoung574458@gmail.com>
2024-11-05 16:25:45 +0900

    create Chatting Mini Project

---
+++ Spring/mini-project/spring-chatting/src/main/java/com/example/springchatting/SpringChattingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springchatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChattingApplication.class, args);
+	}
+
+}
