0a9b0f4fe2a5f5abebc353d785632f50d36d689b
seyoung5744 <seyoung574458@gmail.com>
2024-11-09 14:59:08 +0900

    [Spring Boot] WebSocket과 채팅 (2) - SockJS

--- Spring/mini-project/spring-chatting/src/main/java/com/example/springchatting/config/WebSocketConfig.java
+++ Spring/mini-project/spring-chatting/src/main/java/com/example/springchatting/config/WebSocketConfig.java
@@ -20,6 +20,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry
             .addHandler(chatHandler, "ws/chat")
-            .setAllowedOrigins("*");
+            .setAllowedOrigins("*")
+            .withSockJS();
     }
 }
