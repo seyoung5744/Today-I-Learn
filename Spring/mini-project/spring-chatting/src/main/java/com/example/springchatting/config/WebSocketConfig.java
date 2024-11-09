b01138128f9ef211459f2b201690367082d960d0
seyoung5744 <seyoung574458@gmail.com>
2024-11-09 15:49:13 +0900

    [Spring Boot] WebSocket과 채팅 (3) - STOMP

--- Spring/mini-project/spring-chatting/src/main/java/com/example/springchatting/config/WebSocketConfig.java
+++ Spring/mini-project/spring-chatting/src/main/java/com/example/springchatting/config/WebSocketConfig.java
@@ -1,26 +1,26 @@
 package com.example.springchatting.config;
 
-import com.example.springchatting.handler.ChatHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final ChatHandler chatHandler;
-
-    public WebSocketConfig(ChatHandler chatHandler) {
-        this.chatHandler = chatHandler;
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+            registry
+                .addEndpoint("/stomp/chat")
+                .setAllowedOrigins("http://localhost:8080")
+                .withSockJS();
     }
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry
-            .addHandler(chatHandler, "ws/chat")
-            .setAllowedOrigins("*")
-            .withSockJS();
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/sub");
     }
 }
---
+++ Spring/mini-project/spring-chatting/src/main/java/com/example/springchatting/web/ChatRoomController.java
@@ -0,0 +1,53 @@
+package com.example.springchatting.web;
+
+
+import com.example.springchatting.repository.ChatRoomRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/chat")
+public class ChatRoomController {
+
+    private final ChatRoomRepository repository;
+
+    //채팅방 목록 조회
+    @GetMapping("/rooms")
+    public ModelAndView rooms() {
+
+        log.info("# All Chat Rooms");
+        ModelAndView mv = new ModelAndView("chat/rooms");
+
+        mv.addObject("list", repository.findAllRooms());
+
+        return mv;
+    }
+
+    //채팅방 개설
+    @PostMapping("/room")
+    public String create(@RequestParam String name, RedirectAttributes rttr) {
+
+        log.info("# Create Chat Room , name: " + name);
+        rttr.addFlashAttribute("roomName", repository.createChatRoomDTO(name));
+        return "redirect:/chat/rooms";
+    }
+
+    //채팅방 조회
+    @GetMapping("/room")
+    public void getRoom(String roomId, Model model) {
+
+        log.info("# get Chat Room, roomID : " + roomId);
+
+        model.addAttribute("room", repository.findRoomById(roomId));
+    }
+}
