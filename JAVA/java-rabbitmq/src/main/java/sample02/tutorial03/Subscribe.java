3c73d8f581f658e1ae498e875d73f3f9750aa595
seyoung5744 <seyoung574458@gmail.com>
2024-11-10 17:10:25 +0900

    [RabbitMQ] Tutorials (3)

---
+++ JAVA/java-rabbitmq/src/main/java/sample02/tutorial03/Subscribe.java
@@ -0,0 +1,38 @@
+package sample02.tutorial03;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Subscribe {
+
+    private final static String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] args) throws Exception {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        Connection conn = factory.newConnection();
+        Channel channel = conn.createChannel();
+
+        //exchange
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+        //create queue
+        String queueName = channel.queueDeclare().getQueue();
+        //binding
+        channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+        System.out.println("[*] Waiting for msgs. (Press CTRL + C to exit)");
+
+        DeliverCallback callback = (consumerTag, delivery) -> {
+            String msg = new String(delivery.getBody(), "UTF-8");
+            System.out.println("[x] Received : " + msg);
+        };
+
+        channel.basicConsume(queueName, true, callback, consumerTag -> {
+        });
+    }
+
+}
