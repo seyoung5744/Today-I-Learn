347b367add99f99a3ac1a14b6c41632cfa18f291
seyoung5744 <seyoung574458@gmail.com>
2024-12-01 17:42:39 +0900

    Ch03.서비스 모듈 - 05_Log Aspect 구현

---
+++ Spring/mini-project/fintech/api/src/main/java/com/zerobase/api/aop/LogAspect.java
@@ -0,0 +1,33 @@
+package com.zerobase.api.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Slf4j
+@Aspect
+@Component
+public class LogAspect {
+
+    @Pointcut("within(com.zerobase.api..*)")
+    private void isApi() {
+    }
+
+    @Around("isApi()")
+    public Object loggingAspect(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        Object result = joinPoint.proceed();
+
+        stopWatch.stop();
+
+        log.info("{} {} {}", joinPoint.getSignature().getName(), joinPoint.getArgs()[0], stopWatch.lastTaskInfo().getTimeMillis());
+
+        return result;
+    }
+}
