a8641dcd318b3504c4fca38a14eef21c5a188af3
seyoung5744 <seyoung574458@gmail.com>
2024-12-20 15:40:04 +0900

    변수 - 문제와풀이

---
+++ JAVA/자바 입문 - 코드로 시작하는 자바 첫걸음/java-start/src/variable/ex/VarEx3Question.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class VarEx3Question {
+
+    public static void main(String[] args) {
+        long l = 1000000000L;
+        System.out.println(l);
+        boolean b = true;
+        System.out.println(b);
+    }
+
+}
