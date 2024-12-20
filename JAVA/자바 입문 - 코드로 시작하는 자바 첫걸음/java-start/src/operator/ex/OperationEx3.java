c7cdc599f5b91f07b734f431b87ebf8912b807e2
seyoung5744 <seyoung574458@gmail.com>
2024-12-20 20:29:06 +0900

    연산자 - 문제와 풀이

---
+++ JAVA/자바 입문 - 코드로 시작하는 자바 첫걸음/java-start/src/operator/ex/OperationEx3.java
@@ -0,0 +1,12 @@
+package operator.ex;
+
+public class OperationEx3 {
+
+    public static void main(String[] args) {
+
+        int source = 70;
+        boolean result = source >= 80 && source <= 100;
+        System.out.println(result);
+    }
+
+}
