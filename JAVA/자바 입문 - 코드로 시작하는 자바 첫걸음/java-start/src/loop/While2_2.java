942a151e903f12fe6e72f4d80c6c2468a00c3cde
seyoung5744 <seyoung574458@gmail.com>
2024-12-28 15:03:00 +0900

    while문2

---
+++ JAVA/자바 입문 - 코드로 시작하는 자바 첫걸음/java-start/src/loop/While2_2.java
@@ -0,0 +1,20 @@
+package loop;
+
+public class While2_2 {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 1;
+
+        sum = sum + i;
+        System.out.println("i=" + 1 + " sum=" + sum);
+        i++;
+
+        sum = sum + i;
+        System.out.println("i=" + 2 + " sum=" + sum);
+        i++;
+
+        sum = sum + i;
+        System.out.println("i=" + 3 + " sum=" + sum);
+    }
+}
