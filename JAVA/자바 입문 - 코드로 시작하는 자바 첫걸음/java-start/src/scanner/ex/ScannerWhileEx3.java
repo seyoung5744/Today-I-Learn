6bcb6a2990f2258e2da32aabdb2c49794cfa34b8
seyoung5744 <seyoung574458@gmail.com>
2025-01-02 14:04:50 +0900

    문제 풀이

--- JAVA/자바 입문 - 코드로 시작하는 자바 첫걸음/java-start/src/scanner/ex/ScannerWhileEx3.java
+++ JAVA/자바 입문 - 코드로 시작하는 자바 첫걸음/java-start/src/scanner/ex/ScannerWhileEx3.java
@@ -10,17 +10,22 @@ public class ScannerWhileEx3 {
         System.out.println("숫자를 입력하세요. 입력을 중단하려면 -1을 입력하세요:");
         int sum = 0;
         int count = 0;
-        double average = 0.0;
-        while (true) {
-            int num = sc.nextInt();
-            if (num == -1) {
-                break;
-            }
+//        while (true) {
+//            int num = sc.nextInt();
+//            if (num == -1) {
+//                break;
+//            }
+//            count += 1;
+//            sum += num;
+//        }
+        int num = 0;
+        while ((num = sc.nextInt()) != -1) {
             count += 1;
             sum += num;
         }
+        double average = (double) sum / count;
 
         System.out.println("입력한 숫자들의 합계: " + sum);
-        System.out.println("입력한 숫자들의 평균: " + ((double) sum / count));
+        System.out.println("입력한 숫자들의 평균: " + average);
     }
 }
