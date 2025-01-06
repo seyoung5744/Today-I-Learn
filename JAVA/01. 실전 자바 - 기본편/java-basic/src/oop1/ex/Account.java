0306586abac3322ed0374ca7d0cc6054879efe68
seyoung5744 <seyoung574458@gmail.com>
2025-01-06 20:45:28 +0900

    문제와 풀이

---
+++ JAVA/01. 실전 자바 - 기본편/java-basic/src/oop1/ex/Account.java
@@ -0,0 +1,19 @@
+package oop1.ex;
+
+public class Account {
+
+    int balance;
+
+    void deposit(int amount) {
+        balance += amount;
+    }
+
+    void withdraw(int amount) {
+        if (balance < amount) {
+            System.out.println("잔액이 부족합니다.");
+            return;
+        }
+
+        balance -= amount;
+    }
+}
