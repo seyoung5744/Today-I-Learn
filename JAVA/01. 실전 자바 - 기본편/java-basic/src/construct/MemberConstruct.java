7278283f64b2c13bbe6ce3bb796d92194f3b478b
seyoung5744 <seyoung574458@gmail.com>
2025-01-07 13:55:35 +0900

    생성자 - 오버로딩과 this()

--- JAVA/01. 실전 자바 - 기본편/java-basic/src/construct/MemberConstruct.java
+++ JAVA/01. 실전 자바 - 기본편/java-basic/src/construct/MemberConstruct.java
@@ -6,6 +6,15 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    // 추가
+    MemberConstruct(String name, int age) {
+//        System.out.println("go");
+        this(name, age, 50); // Call to 'this()' must be first statement in constructor body
+//        this.name = name;
+//        this.age = age;
+//        this.grade = 50;
+    }
+
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name=" + name + ", age=" + age + ", grade=" + grade);
         this.name = name;
