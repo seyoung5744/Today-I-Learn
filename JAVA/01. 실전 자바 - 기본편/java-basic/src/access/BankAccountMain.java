93d8afcf44da80250e1e061ea06eaceaacad1342
seyoung5744 <seyoung574458@gmail.com>
2025-01-07 17:19:23 +0900

    캡슐화

---
+++ JAVA/01. 실전 자바 - 기본편/java-basic/src/access/BankAccountMain.java
@@ -0,0 +1,12 @@
+package access;
+
+public class BankAccountMain {
+
+    public static void main(String[] args) {
+        BackAccount account = new BackAccount();
+        account.deposit(10000);
+        account.withdraw(3000);
+        System.out.println("balance = " + account.getBalance());
+    }
+
+}
