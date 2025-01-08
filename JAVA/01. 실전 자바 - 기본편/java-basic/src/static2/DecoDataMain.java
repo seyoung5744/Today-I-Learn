868a8204964cabbebca250f5805de508e045d99c
seyoung5744 <seyoung574458@gmail.com>
2025-01-08 17:24:34 +0900

    static 메서드3

--- JAVA/01. 실전 자바 - 기본편/java-basic/src/static2/DecoDataMain.java
+++ JAVA/01. 실전 자바 - 기본편/java-basic/src/static2/DecoDataMain.java
@@ -1,10 +1,16 @@
 package static2;
 
+import static static2.DecoData.staticCall;
+
 public class DecoDataMain {
 
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
-        DecoData.staticCall();
+        staticCall();
+        staticCall();
+        staticCall();
+        staticCall();
+        staticCall();
 
         System.out.println("2. 인스턴스 호출1");
         DecoData data1 = new DecoData();
@@ -14,7 +20,15 @@ public class DecoDataMain {
         DecoData data2 = new DecoData();
         data2.instanceCall();
 
-        DecoData.staticCall(data1);
+        staticCall(data1);
+
+        // 추가
+        // 인스턴스를 통한 접근
+        DecoData data3 = new DecoData();
+        staticCall();
+
+        // 클래스를 통한 접근
+        staticCall();
     }
 
 }
