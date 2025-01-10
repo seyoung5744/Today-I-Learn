5759604dfe2036980eb6db7fe2871fe48cc3e6f4
seyoung5744 <seyoung574458@gmail.com>
2025-01-10 13:50:25 +0900

    문제와 풀이

---
+++ JAVA/01. 실전 자바 - 기본편/java-basic/src/extend1/ex/Book.java
@@ -0,0 +1,19 @@
+package extend1.ex;
+
+public class Book extends Item {
+
+    private final String author;
+    private final String isbn;
+
+    public Book(String name, int price, String author, String isbn) {
+        super(name, price);
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("- 저자: " + author + ", isbn: " + isbn);
+    }
+}
