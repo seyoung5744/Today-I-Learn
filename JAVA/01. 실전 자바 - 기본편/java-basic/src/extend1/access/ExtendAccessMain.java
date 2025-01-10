7eb7045f4a47ca4ca0ac15a634e9028d5085d21d
seyoung5744 <seyoung574458@gmail.com>
2025-01-10 12:44:54 +0900

    상속과 접근 제어

---
+++ JAVA/01. 실전 자바 - 기본편/java-basic/src/extend1/access/ExtendAccessMain.java
@@ -0,0 +1,12 @@
+package extend1.access;
+
+import extend1.access.child.Child;
+
+public class ExtendAccessMain {
+
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.call();
+    }
+
+}
