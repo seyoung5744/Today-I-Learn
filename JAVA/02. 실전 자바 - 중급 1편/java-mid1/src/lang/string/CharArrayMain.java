0f1a081e8bef0a3ef81ca3feb6ee8ab992da704a
seyoung5744 <seyoung574458@gmail.com>
2025-01-16 17:05:05 +0900

    String 클래스 - 기본

---
+++ JAVA/02. 실전 자바 - 중급 1편/java-mid1/src/lang/string/CharArrayMain.java
@@ -0,0 +1,16 @@
+package lang.string;
+
+public class CharArrayMain {
+
+    public static void main(String[] args) {
+        char a = 'a';
+        System.out.println("a = " + a);
+
+        char[] charArr = new char[]{'h', 'e', 'l', 'l', 'o' };
+        System.out.println(charArr);
+
+        String str = "hello";
+        System.out.println(str);
+    }
+
+}
