20c62a5d1cc58b3a5b72d9a2f54f956d119b57c0
seyoung5744 <seyoung574458@gmail.com>
2025-01-23 20:19:06 +0900

    문제와 풀이

---
+++ JAVA/02. 실전 자바 - 중급 1편/java-mid1/src/nested/test/OuterClass2.java
@@ -0,0 +1,14 @@
+package nested.test;
+
+public class OuterClass2 {
+
+    class InnerClass {
+        public void hello() {
+            System.out.println("InnerClass.hello");
+        }
+
+        public void test() {
+            
+        }
+    }
+}
