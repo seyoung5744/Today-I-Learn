8704b74f31ef3693733f060b1a0a7f62d0558433
seyoung5744 <seyoung574458@gmail.com>
2025-01-24 21:28:32 +0900

    예외 처리 도입2 - 예외 복구

--- JAVA/02. 실전 자바 - 중급 1편/java-mid1/src/exception/ex2/MainV2.java
+++ JAVA/02. 실전 자바 - 중급 1편/java-mid1/src/exception/ex2/MainV2.java
@@ -7,7 +7,8 @@ public class MainV2 {
     public static void main(String[] args) throws NetworkClientExceptionV2 {
         Scanner sc = new Scanner(System.in);
 
-        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+//        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+        NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
 
         while (true) {
             System.out.print("전송할 문자: ");
