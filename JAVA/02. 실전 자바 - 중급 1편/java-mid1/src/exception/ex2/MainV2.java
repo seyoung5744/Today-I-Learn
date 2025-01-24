cabd71c7950e5bb327934d4ee6c0ed1974a7ae15
seyoung5744 <seyoung574458@gmail.com>
2025-01-24 21:04:05 +0900

    예외 처리 도입1 - 시작

---
+++ JAVA/02. 실전 자바 - 중급 1편/java-mid1/src/exception/ex2/MainV2.java
@@ -0,0 +1,25 @@
+package exception.ex2;
+
+import java.util.Scanner;
+
+public class MainV2 {
+
+    public static void main(String[] args) throws NetworkClientExceptionV2 {
+        Scanner sc = new Scanner(System.in);
+
+        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+
+        while (true) {
+            System.out.print("전송할 문자: ");
+            String input = sc.nextLine();
+
+            if (input.equals("exit")) {
+                break;
+            }
+
+            networkService.sendMessage(input);
+            System.out.println();
+        }
+        System.out.println("프로그램을 정상 종료합니다.");
+    }
+}
