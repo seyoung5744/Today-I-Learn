49e3bccff86e56b92d6524f70c1d4b1fce895312
seyoung5744 <seyoung574458@gmail.com>
2025-01-24 21:44:46 +0900

    예외 처리 도입3 - 정상, 예외 흐름 분리

--- JAVA/02. 실전 자바 - 중급 1편/java-mid1/src/exception/ex2/MainV2.java
+++ JAVA/02. 실전 자바 - 중급 1편/java-mid1/src/exception/ex2/MainV2.java
@@ -8,7 +8,8 @@ public class MainV2 {
         Scanner sc = new Scanner(System.in);
 
 //        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
-        NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
+//        NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
+        NetworkServiceV2_3 networkService = new NetworkServiceV2_3();
 
         while (true) {
             System.out.print("전송할 문자: ");
