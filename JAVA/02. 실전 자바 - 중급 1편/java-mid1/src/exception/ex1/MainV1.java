d85066ecf4810432cacf1c9fbf1737591ab02f31
seyoung5744 <seyoung574458@gmail.com>
2025-01-24 16:27:23 +0900

    예외 처리가 필요한 이유2 - 오류 상황 만들기

---
+++ JAVA/02. 실전 자바 - 중급 1편/java-mid1/src/exception/ex1/MainV1.java
@@ -0,0 +1,26 @@
+package exception.ex1;
+
+
+import java.util.Scanner;
+
+public class MainV1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        NetworkServiceV1_1 networkService = new NetworkServiceV1_1();
+
+        while (true) {
+            System.out.print("전송할 문자: ");
+            String input = sc.nextLine();
+
+            if (input.equals("exit")) {
+                break;
+            }
+
+            networkService.sendMessage(input);
+            System.out.println();
+        }
+        System.out.println("프로그램을 정상 종료합니다.");
+    }
+}
