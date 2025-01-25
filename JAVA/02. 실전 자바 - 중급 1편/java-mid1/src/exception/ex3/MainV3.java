092116ff521c3728d6f837179a8e29917caabfef
seyoung5744 <seyoung574458@gmail.com>
2025-01-25 15:16:46 +0900

    예외 계층1 - 시작

---
+++ JAVA/02. 실전 자바 - 중급 1편/java-mid1/src/exception/ex3/MainV3.java
@@ -0,0 +1,26 @@
+package exception.ex3;
+
+
+import java.util.Scanner;
+
+public class MainV3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        NetworkServiceV3_1 networkService = new NetworkServiceV3_1();
+
+        while (true) {
+            System.out.print("전송할 문자: ");
+            String input = sc.nextLine();
+
+            if (input.equals("exit")) {
+                break;
+            }
+
+            networkService.sendMessage(input);
+            System.out.println();
+        }
+        System.out.println("프로그램을 정상 종료합니다.");
+    }
+}
