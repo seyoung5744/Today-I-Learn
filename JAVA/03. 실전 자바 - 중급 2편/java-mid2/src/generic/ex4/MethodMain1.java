c4e761f34ed36b326fc0e003ce78c0ef68436eae
seyoung5744 <seyoung574458@gmail.com>
2025-01-26 19:56:52 +0900

    제네릭 메서드

---
+++ JAVA/03. 실전 자바 - 중급 2편/java-mid2/src/generic/ex4/MethodMain1.java
@@ -0,0 +1,18 @@
+package generic.ex4;
+
+public class MethodMain1 {
+
+    public static void main(String[] args) {
+        Integer i = 20;
+        Object object = GenericMethod.objMethod(i);
+        Integer result = (Integer) GenericMethod.objMethod(i);
+
+        // 타입 인자(Type Argument) 명시적 전달
+        System.out.println("명시적 타입 인자 전달");
+        Integer result1 = GenericMethod.<Integer>genericMethod(i);
+        Integer integerValue = GenericMethod.<Integer>numberMethod(10);
+        Double doubleValue = GenericMethod.<Double>numberMethod(20.0);
+
+
+    }
+}
