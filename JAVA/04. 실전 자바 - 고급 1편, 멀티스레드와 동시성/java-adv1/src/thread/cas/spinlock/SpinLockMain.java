0615ebc273a33f010ebe379f23948929ee57885c
seyoung5744 <seyoung574458@gmail.com>
2025-02-20 16:31:38 +0900

    CAS 락 구현1

---
+++ JAVA/04. 실전 자바 - 고급 1편, 멀티스레드와 동시성/java-adv1/src/thread/cas/spinlock/SpinLockMain.java
@@ -0,0 +1,30 @@
+package thread.cas.spinlock;
+
+import static util.MyLogger.log;
+
+public class SpinLockMain {
+
+    public static void main(String[] args) {
+        SpinLockBad spinLock = new SpinLockBad();
+
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                spinLock.lock();
+                try {
+                    // critical section
+                    log("비즈니스 로직 실행");
+                } finally {
+                    spinLock.unlock();
+                }
+            }
+        };
+
+        Thread thread1 = new Thread(task, "Thread-1");
+        Thread thread2 = new Thread(task, "Thread-1");
+
+        thread1.start();
+        thread2.start();
+
+    }
+}
