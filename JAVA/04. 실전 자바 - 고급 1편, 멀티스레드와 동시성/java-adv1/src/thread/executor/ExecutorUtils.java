b31ddac13296dd1f29e6c90fc992fc557b6061ce
seyoung5744 <seyoung574458@gmail.com>
2025-02-24 14:56:47 +0900

    Executor 스레드 풀 관리 - 코드

--- JAVA/04. 실전 자바 - 고급 1편, 멀티스레드와 동시성/java-adv1/src/thread/executor/ExecutorUtils.java
+++ JAVA/04. 실전 자바 - 고급 1편, 멀티스레드와 동시성/java-adv1/src/thread/executor/ExecutorUtils.java
@@ -8,15 +8,24 @@ import static util.MyLogger.log;
 public abstract class ExecutorUtils {
 
     public static void printState(ExecutorService executorService) {
+        printState(executorService, null);
+    }
+
+    public static void printState(ExecutorService executorService, String taskName) {
         if (executorService instanceof ThreadPoolExecutor poolExecutor) {
-            int pool = poolExecutor.getPoolSize();
-            int active = poolExecutor.getActiveCount();
-            int queuedTasks = poolExecutor.getQueue().size();
-            long completedTask = poolExecutor.getCompletedTaskCount();
-            log("[pool=" + pool + ", active=" + active + ", queuedTasks=" + queuedTasks + ", completedTask=" + completedTask + "]");
+            String message = getStateMessage(poolExecutor);
+            log(taskName == null ? message : taskName + " -> " + message);
         } else {
             log(executorService);
         }
+    }
+
+    private static String getStateMessage(ThreadPoolExecutor poolExecutor) {
+        int pool = poolExecutor.getPoolSize();
+        int active = poolExecutor.getActiveCount();
+        int queuedTasks = poolExecutor.getQueue().size();
+        long completedTask = poolExecutor.getCompletedTaskCount();
+        return "[pool=" + pool + ", active=" + active + ", queuedTasks=" + queuedTasks + ", completedTask=" + completedTask + "]";
     }
 
 }
