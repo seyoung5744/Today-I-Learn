ea3fb5dcb68704b9b6bc1f41f40f96164fbcd82f
seyoung5744 <seyoung574458@gmail.com>
2025-03-06 20:26:41 +0900

    네트워크 예외4 - 강제 종료

---
+++ JAVA/05. 실전 자바 - 고급 2편, IO, 네트워크, 리플렉션/java-adv2/src/network/exception/close/reset/ResetCloseClient.java
@@ -0,0 +1,48 @@
+package network.exception.close.reset;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.SocketException;
+
+import static util.MyLogger.log;
+
+public class ResetCloseClient {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Socket socket = new Socket("localhost", 12345);
+        log("소켓 연결: " + socket);
+        InputStream input = socket.getInputStream();
+        OutputStream output = socket.getOutputStream();
+
+//        input.close();
+//        output.close();
+//        socket.close();
+
+        // client <- server: FIN
+        Thread.sleep(1000); // 서버가 close() 호출할 때 까지 대기
+
+        // client -> server: PUSH[1]
+        output.write(1);
+
+        // client <- server: RST
+        Thread.sleep(1000); // RST 메세지 전송 대기
+
+        try {
+            // java.net.SocketException: Connection reset
+            // 윈도우의 경우 메세지가 다름
+            int read = input.read();
+            System.out.println("read = " + read);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            // java.net.SocketException: Broken pipe
+            output.write(1);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+}
