1b7bf3ad3625b33b7cdc476ee68fce25be27ad0f
seyoung5744 <seyoung574458@gmail.com>
2025-03-13 16:19:09 +0900

    HTTP 서버9 - 애노테이션 서블릿3 - 성능 최적화

--- JAVA/05. 실전 자바 - 고급 2편, IO, 네트워크, 리플렉션/java-adv2/src/was/v8/ServerMainV8.java
+++ JAVA/05. 실전 자바 - 고급 2편, IO, 네트워크, 리플렉션/java-adv2/src/was/v8/ServerMainV8.java
@@ -1,9 +1,11 @@
 package was.v8;
 
 import was.httpserver.HttpServer;
+import was.httpserver.HttpServlet;
 import was.httpserver.ServletManager;
 import was.httpserver.servlet.DiscardServlet;
 import was.httpserver.servlet.annotation.AnnotationServletV2;
+import was.httpserver.servlet.annotation.AnnotationServletV3;
 
 import java.io.IOException;
 import java.util.List;
@@ -14,7 +16,8 @@ public class ServerMainV8 {
 
     public static void main(String[] args) throws IOException {
         List<Object> controllers = List.of(new SiteControllerV8(), new SearchControllerV8());
-        AnnotationServletV2 annotationServlet = new AnnotationServletV2(controllers);
+//        HttpServlet annotationServlet = new AnnotationServletV2(controllers);
+        HttpServlet annotationServlet = new AnnotationServletV3(controllers);
 
         ServletManager servletManager = new ServletManager();
         servletManager.setDefaultServlet(annotationServlet);
