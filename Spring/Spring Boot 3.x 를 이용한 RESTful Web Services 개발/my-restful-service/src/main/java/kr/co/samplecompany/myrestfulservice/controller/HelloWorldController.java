d15b6ed66164f100f55d77bd3fb50537d133a156
seyoung5744 <seyoung574458@gmail.com>
2025-03-24 15:38:56 +0900

    Path Variable 이용 - 가변 URI 사용

--- Spring/Spring Boot 3.x 를 이용한 RESTful Web Services 개발/my-restful-service/src/main/java/kr/co/samplecompany/myrestfulservice/controller/HelloWorldController.java
+++ Spring/Spring Boot 3.x 를 이용한 RESTful Web Services 개발/my-restful-service/src/main/java/kr/co/samplecompany/myrestfulservice/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package kr.co.samplecompany.myrestfulservice.controller;
 
 import kr.co.samplecompany.myrestfulservice.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +19,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World!");
+    }
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
