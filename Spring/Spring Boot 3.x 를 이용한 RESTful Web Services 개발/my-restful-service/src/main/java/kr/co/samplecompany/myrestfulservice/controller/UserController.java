957e1c970c1e90bae0697b605881d3a946e92a31
seyoung5744 <seyoung574458@gmail.com>
2025-03-25 15:05:52 +0900

    HTTP Status Code 제어

--- Spring/Spring Boot 3.x 를 이용한 RESTful Web Services 개발/my-restful-service/src/main/java/kr/co/samplecompany/myrestfulservice/controller/UserController.java
+++ Spring/Spring Boot 3.x 를 이용한 RESTful Web Services 개발/my-restful-service/src/main/java/kr/co/samplecompany/myrestfulservice/controller/UserController.java
@@ -2,8 +2,11 @@ package kr.co.samplecompany.myrestfulservice.controller;
 
 import kr.co.samplecompany.myrestfulservice.bean.User;
 import kr.co.samplecompany.myrestfulservice.dao.UserDaoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,7 +29,14 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public User createUser(@RequestBody User user) {
-        return service.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
