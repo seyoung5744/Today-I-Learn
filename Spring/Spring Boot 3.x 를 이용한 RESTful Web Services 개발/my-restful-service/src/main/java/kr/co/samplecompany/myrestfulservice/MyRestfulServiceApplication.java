0533212f0fa074803845233a9a2e74d343748b0e
seyoung5744 <seyoung574458@gmail.com>
2025-03-25 17:13:03 +0900

    다국어 처리를 위한 Internationalization 구현 방법

--- Spring/Spring Boot 3.x 를 이용한 RESTful Web Services 개발/my-restful-service/src/main/java/kr/co/samplecompany/myrestfulservice/MyRestfulServiceApplication.java
+++ Spring/Spring Boot 3.x 를 이용한 RESTful Web Services 개발/my-restful-service/src/main/java/kr/co/samplecompany/myrestfulservice/MyRestfulServiceApplication.java
@@ -2,6 +2,11 @@ package kr.co.samplecompany.myrestfulservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class MyRestfulServiceApplication {
@@ -15,4 +20,10 @@ public class MyRestfulServiceApplication {
 //        }
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
 }
--- Spring/Spring Boot 3.x 를 이용한 RESTful Web Services 개발/my-restful-service/src/main/java/kr/co/samplecompany/myrestfulservice/controller/HelloWorldController.java
+++ Spring/Spring Boot 3.x 를 이용한 RESTful Web Services 개발/my-restful-service/src/main/java/kr/co/samplecompany/myrestfulservice/controller/HelloWorldController.java
@@ -1,12 +1,22 @@
 package kr.co.samplecompany.myrestfulservice.controller;
 
 import kr.co.samplecompany.myrestfulservice.bean.HelloWorldBean;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    private final MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     // GET
     // URI - /hello-world
@@ -25,4 +35,11 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
+    }
+
 }
