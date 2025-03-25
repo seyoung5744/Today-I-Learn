ceadd3767825c51c44235e91d300e9a197e591db
seyoung5744 <seyoung574458@gmail.com>
2025-03-25 20:12:30 +0900

    프로그래밍으로 제어하는 Filtering - 개별 사용자 조회

---
+++ Spring/Spring Boot 3.x 를 이용한 RESTful Web Services 개발/my-restful-service/src/main/java/kr/co/samplecompany/myrestfulservice/controller/AdminUserController.java
@@ -0,0 +1,48 @@
+package kr.co.samplecompany.myrestfulservice.controller;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import kr.co.samplecompany.myrestfulservice.bean.AdminUser;
+import kr.co.samplecompany.myrestfulservice.bean.User;
+import kr.co.samplecompany.myrestfulservice.dao.UserDaoService;
+import kr.co.samplecompany.myrestfulservice.exception.UserNotFoundException;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminUserController {
+
+    private final UserDaoService service;
+
+    public AdminUserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users/{id}")
+    public MappingJacksonValue retrieveUser4Admin(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        AdminUser adminUser = new AdminUser();
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        // adminUser.setName(user.getName()); 와 같이 속성을 일일이 복사해줄 작업을 대신해주는 utils
+        BeanUtils.copyProperties(user, adminUser);
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+}
