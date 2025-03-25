c19a1fa2a00ece9f79835326323bd5a3bf291463
seyoung5744 <seyoung574458@gmail.com>
2025-03-25 14:01:38 +0900

    User 도메인 클래스 생성

---
+++ Spring/Spring Boot 3.x 를 이용한 RESTful Web Services 개발/my-restful-service/src/main/java/kr/co/samplecompany/myrestfulservice/bean/User.java
@@ -0,0 +1,15 @@
+package kr.co.samplecompany.myrestfulservice.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    private Integer id;
+    private String name;
+    private Date joinDate;
+}
---
+++ Spring/Spring Boot 3.x 를 이용한 RESTful Web Services 개발/my-restful-service/src/main/java/kr/co/samplecompany/myrestfulservice/dao/UserDaoService.java
@@ -0,0 +1,44 @@
+package kr.co.samplecompany.myrestfulservice.dao;
+
+import kr.co.samplecompany.myrestfulservice.bean.User;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+
+    private static List<User> users = new ArrayList<>();
+
+    private static int userCount = 3;
+
+    static {
+        users.add(new User(1, "Kenneth", new Date()));
+        users.add(new User(2, "Alice", new Date()));
+        users.add(new User(3, "Elena", new Date()));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        if (user.getId() == null) {
+            user.setId(++userCount);
+        }
+
+        users.add(user);
+        return user;
+    }
+
+    public User findOne(Integer id) {
+        for (User user : users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
+    }
+}
