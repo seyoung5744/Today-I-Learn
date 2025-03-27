e003cac2c3cf16b86dd1d52ac8dbbd1d571025ad
seyoung5744 <seyoung574458@gmail.com>
2025-03-27 17:22:26 +0900

    Spring Data JPA를 이용한 초기 데이터 생성

---
+++ Spring/Spring Boot 3.x 를 이용한 RESTful Web Services 개발/my-restful-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users(id, join_date, name, password, ssn) values (90001, now(), 'User1', 'test111', '111111-1111111');
+insert into users(id, join_date, name, password, ssn) values (90002, now(), 'User2', 'test222', '222222-2222222');
+insert into users(id, join_date, name, password, ssn) values (90003, now(), 'User3', 'test333', '333333-3333333');
