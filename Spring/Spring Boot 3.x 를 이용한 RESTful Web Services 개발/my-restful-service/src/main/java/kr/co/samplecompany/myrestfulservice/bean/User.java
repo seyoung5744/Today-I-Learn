41490a660692ffbde7d005099cd697eb68eaf49d
seyoung5744 <seyoung574458@gmail.com>
2025-03-27 17:09:09 +0900

    JPA 사용을 위한 Dependency 추가와 Entity 설정

--- Spring/Spring Boot 3.x 를 이용한 RESTful Web Services 개발/my-restful-service/src/main/java/kr/co/samplecompany/myrestfulservice/bean/User.java
+++ Spring/Spring Boot 3.x 를 이용한 RESTful Web Services 개발/my-restful-service/src/main/java/kr/co/samplecompany/myrestfulservice/bean/User.java
@@ -2,20 +2,30 @@ package kr.co.samplecompany.myrestfulservice.bean;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(value = {"password", "ssn"})
 @Schema(description = "사용자 상제 정보를 위한 도메인 객체")
+@Entity
+@Table(name = "USERS")
 public class User {
 
     @Schema(title = "사용자 ID", description = "사용자 ID는 자동 생성됩니다.")
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Schema(title = "사용자 이름", description = "사용자 이름을 입력해주세요")
