d735404f2a65aef68a37b958582e8ec8acef2504
seyoung5744 <seyoung574458@gmail.com>
2025-03-29 20:32:39 +0900

    HTTP 요청 파라미터 - @ModelAttribute

---
+++ Spring/01. 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/Spring_MVC_V1/springmvc/src/main/java/hello/springmvc/basic/HelloData.java
@@ -0,0 +1,37 @@
+package hello.springmvc.basic;
+
+public class HelloData {
+
+    private String username;
+    private int age;
+
+    public HelloData() {
+        System.out.println("HelloData.HelloData");
+    }
+
+    public HelloData(String username, int age) {
+        System.out.println("HelloData.HelloData(String, int)");
+        this.username = username;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        System.out.println("HelloData.getUsername");
+        return username;
+    }
+
+    public void setUsername(String username) {
+        System.out.println("HelloData.setUsername");
+        this.username = username;
+    }
+
+    public int getAge() {
+        System.out.println("HelloData.getAge");
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("HelloData.setAge");
+        this.age = age;
+    }
+}
