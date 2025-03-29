c1d2947ce5ed0d200e6e0eab01c31e2c679b5b9f
seyoung5744 <seyoung574458@gmail.com>
2025-03-29 21:23:02 +0900

    HTTP 요청 메시지 - JSON

--- Spring/01. 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/Spring_MVC_V1/springmvc/src/main/java/hello/springmvc/basic/HelloData.java
+++ Spring/01. 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/Spring_MVC_V1/springmvc/src/main/java/hello/springmvc/basic/HelloData.java
@@ -9,29 +9,29 @@ public class HelloData {
         System.out.println("HelloData.HelloData");
     }
 
-    public HelloData(String username, int age) {
-        System.out.println("HelloData.HelloData(String, int)");
-        this.username = username;
-        this.age = age;
-    }
+//    public HelloData(String username, int age) {
+//        System.out.println("HelloData.HelloData(String, int)");
+//        this.username = username;
+//        this.age = age;
+//    }
 
     public String getUsername() {
         System.out.println("HelloData.getUsername");
         return username;
     }
 
-    public void setUsername(String username) {
-        System.out.println("HelloData.setUsername");
-        this.username = username;
-    }
+//    public void setUsername(String username) {
+//        System.out.println("HelloData.setUsername");
+//        this.username = username;
+//    }
 
     public int getAge() {
         System.out.println("HelloData.getAge");
         return age;
     }
 
-    public void setAge(int age) {
-        System.out.println("HelloData.setAge");
-        this.age = age;
-    }
+//    public void setAge(int age) {
+//        System.out.println("HelloData.setAge");
+//        this.age = age;
+//    }
 }
