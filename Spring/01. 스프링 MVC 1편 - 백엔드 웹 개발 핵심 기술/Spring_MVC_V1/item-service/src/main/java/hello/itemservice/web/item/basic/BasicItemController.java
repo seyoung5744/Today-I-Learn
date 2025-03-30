4458384c3353ba6639aec8d86be26a41bfe107cc
seyoung5744 <seyoung574458@gmail.com>
2025-03-30 15:23:23 +0900

    상품 상세

--- Spring/01. 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/Spring_MVC_V1/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ Spring/01. 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/Spring_MVC_V1/item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,10 +20,17 @@ public class BasicItemController {
     private final ItemRepository itemRepository;
 
     @GetMapping
-    public String item(Model model) {
+    public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
 
     @PostConstruct
