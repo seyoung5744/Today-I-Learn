71cbfb89d4a89271929526895633ef1687a10138
seyoung5744 <seyoung574458@gmail.com>
2025-04-01 14:42:25 +0900

    Bean Validation - groups

--- Spring/02. 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/Spring_MVC_V2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ Spring/02. 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/Spring_MVC_V2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -9,17 +9,18 @@ import org.hibernate.validator.constraints.Range;
 @Data
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank(message = "공백! {0}")
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
