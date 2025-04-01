458194caf9d52477020029d98b54ab874e1cd3f3
seyoung5744 <seyoung574458@gmail.com>
2025-04-01 15:36:27 +0900

    Bean Validation - HTTP 메시지 컨버터

--- Spring/02. 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/Spring_MVC_V2/validation/src/main/java/hello/itemservice/web/validation/form/ItemSaveForm.java
+++ Spring/02. 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/Spring_MVC_V2/validation/src/main/java/hello/itemservice/web/validation/form/ItemSaveForm.java
@@ -4,9 +4,13 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Range;
 
-@Data
+//@Data
+//@Setter
+@Getter
 public class ItemSaveForm {
 
     @NotBlank
@@ -19,4 +23,30 @@ public class ItemSaveForm {
     @NotNull
     @Max(9999)
     private Integer quantity;
+
+    public ItemSaveForm() {
+        System.out.println("ItemSaveForm.ItemSaveForm");
+    }
+
+//    public ItemSaveForm(String itemName, Integer price, Integer quantity) {
+//        System.out.println("ItemSaveForm.ItemSaveForm(String, Integer, Integer)");
+//        this.itemName = itemName;
+//        this.price = price;
+//        this.quantity = quantity;
+//    }
+
+//    public void setItemName(String itemName) {
+//        System.out.println("ItemSaveForm.setItemName");
+//        this.itemName = itemName;
+//    }
+//
+//    public void setPrice(Integer price) {
+//        System.out.println("ItemSaveForm.setPrice");
+//        this.price = price;
+//    }
+//
+//    public void setQuantity(Integer quantity) {
+//        System.out.println("ItemSaveForm.setQuantity");
+//        this.quantity = quantity;
+//    }
 }
