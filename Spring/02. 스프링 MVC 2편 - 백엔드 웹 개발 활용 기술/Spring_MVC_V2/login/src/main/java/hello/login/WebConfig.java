007c64485b60d2f59b569b36d1d02b95cc4415e7
seyoung5744 <seyoung574458@gmail.com>
2025-04-01 21:56:28 +0900

    ArgumentResolver 활용

--- Spring/02. 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/Spring_MVC_V2/login/src/main/java/hello/login/WebConfig.java
+++ Spring/02. 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/Spring_MVC_V2/login/src/main/java/hello/login/WebConfig.java
@@ -1,5 +1,6 @@
 package hello.login;
 
+import hello.login.web.argumentresolver.LoginMemberArgumentResolver;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
@@ -7,8 +8,11 @@ import hello.login.web.interceptor.LoginCheckInterceptor;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -24,6 +28,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(2)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/", "/member/add", "/login", "/logout", "/css/**", "/*.ico", "/error");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
     }
 
     //    @Bean
