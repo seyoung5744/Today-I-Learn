4ed5a6dbaca9ff26e754ab122a4be9ee2e777b29
seyoung5744 <seyoung574458@gmail.com>
2025-04-03 17:34:23 +0900

    API 예외 처리 - HandlerExceptionResolver 활용

--- Spring/02. 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/Spring_MVC_V2/exception/src/main/java/hello/exception/WebConfig.java
+++ Spring/02. 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/Spring_MVC_V2/exception/src/main/java/hello/exception/WebConfig.java
@@ -3,6 +3,7 @@ package hello.exception;
 import hello.exception.filter.LogFilter;
 import hello.exception.interceptor.LogInterceptor;
 import hello.exception.resolver.MyHandlerExceptionResolver;
+import hello.exception.resolver.UserHandlerExceptionResolver;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -24,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 .excludePathPatterns(
                         "/css/**", "/*.ico",
-                        "/error",
+//                        "/error",
                         "/error-page/**" //오류 페이지 경로
                 );
     }
@@ -32,6 +33,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
         resolvers.add(new MyHandlerExceptionResolver());
+        resolvers.add(new UserHandlerExceptionResolver());
     }
 
 //    @Bean
