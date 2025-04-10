52a7c100e8c95766f3812f05edda625750551bd7
seyoung5744 <seyoung574458@gmail.com>
2025-04-10 15:15:02 +0900

    MyBatis 적용2 - 설정과 실행

--- Spring/04. 스프링 DB 2편 - 데이터 접근 활용 기술/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ Spring/04. 스프링 DB 2편 - 데이터 접근 활용 기술/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,6 @@
 package hello.itemservice;
 
-import hello.itemservice.config.JdbcTemplateV3Config;
+import hello.itemservice.config.MyBatisConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -8,15 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 @Slf4j
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
-@Import(JdbcTemplateV3Config.class)
+//@Import(JdbcTemplateV3Config.class)
+@Import(MyBatisConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
