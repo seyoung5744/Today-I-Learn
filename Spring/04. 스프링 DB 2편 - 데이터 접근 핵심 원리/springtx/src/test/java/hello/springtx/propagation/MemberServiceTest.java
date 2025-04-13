a1d9cc9c5a5bb4710cdde66515b903e9b939ff40
seyoung5744 <seyoung574458@gmail.com>
2025-04-13 19:59:33 +0900

    트랜잭션 전파 활용6 - 복구 REQUIRED

--- Spring/04. 스프링 DB 2편 - 데이터 접근 핵심 원리/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ Spring/04. 스프링 DB 2편 - 데이터 접근 핵심 원리/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 @SpringBootTest
@@ -111,6 +113,25 @@ class MemberServiceTest {
         // then: 모든 데이터가 정상 저장된다.
         assertThat(memberRepository.find(username)).isEmpty();
         assertThat(logRepository.find(username)).isEmpty();
+    }
+
+    /**
+     * MemberService @Transactional:ON
+     * MemberRepository @Transactional:ON
+     * LogRepository @Transactional:ON Exception
+     */
+    @Test
+    void recoverException_fail() {
+        // given
+        String username = "로그예외_recoverException_fail";
+
+        // when
+        assertThatThrownBy(() -> memberService.joinV2(username))
+                .isInstanceOf(UnexpectedRollbackException.class);
+
+        // then: 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
     }
 
 }
