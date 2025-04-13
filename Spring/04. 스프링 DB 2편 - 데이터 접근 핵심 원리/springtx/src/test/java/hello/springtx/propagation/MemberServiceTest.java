66308c87703a52d29307e63547f7b259c1ae465a
seyoung5744 <seyoung574458@gmail.com>
2025-04-13 17:23:09 +0900

    트랜잭션 전파 활용2 - 커밋, 롤백

--- Spring/04. 스프링 DB 2편 - 데이터 접근 핵심 원리/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ Spring/04. 스프링 DB 2편 - 데이터 접근 핵심 원리/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -1,13 +1,12 @@
 package hello.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
@@ -39,5 +38,25 @@ class MemberServiceTest {
         assertThat(memberRepository.find(username)).isPresent();
         assertThat(logRepository.find(username)).isPresent();
     }
+
+    /**
+     * MemberService @Transactional:OFF
+     * MemberRepository @Transactional:ON
+     * LogRepository @Transactional:ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        // given
+        String username = "로그예외_outerTxOff_fail";
+
+        // when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        // then: 완전히 롤백되지 않고, member 데이터가 남아서 저장된다.
+        assertThat(memberRepository.find(username)).isPresent();
+        assertThat(logRepository.find(username)).isEmpty();
+    }
+
 
 }
