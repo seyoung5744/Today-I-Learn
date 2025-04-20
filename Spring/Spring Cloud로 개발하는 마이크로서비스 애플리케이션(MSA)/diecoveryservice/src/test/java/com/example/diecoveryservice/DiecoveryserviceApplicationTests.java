3dc89d35f706ce604b62f3fcd82a81b3b6509db3
seyoung5744 <seyoung574458@gmail.com>
2025-04-20 17:30:46 +0900

    Eureka Service Discovery - 프로젝트 생성

---
+++ Spring/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/diecoveryservice/src/test/java/com/example/diecoveryservice/DiecoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diecoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiecoveryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
