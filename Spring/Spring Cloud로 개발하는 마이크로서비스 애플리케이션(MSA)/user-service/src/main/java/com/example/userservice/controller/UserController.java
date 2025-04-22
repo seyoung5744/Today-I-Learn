5079e9c821852aecd6be61e394463f9aa85e235d
seyoung5744 <seyoung574458@gmail.com>
2025-04-22 20:09:42 +0900

    Users Microservice - JPA1

--- Spring/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ Spring/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,24 +1,27 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
+import com.example.userservice.vo.CreateUserRequest;
 import com.example.userservice.vo.Greeting;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequestMapping
 @RestController
+@RequiredArgsConstructor
 public class UserController {
 
     private final Environment env;
     private final Greeting greeting;
 
-    public UserController(Environment env, Greeting greeting) {
-        this.env = env;
-        this.greeting = greeting;
-    }
+    private final UserService userService;
+
 
     @GetMapping("/health-check")
     public String status() {
@@ -29,5 +32,17 @@ public class UserController {
     public String welcome() {
 //        return env.getProperty("greeting.message");
         return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public String createUser(@RequestBody CreateUserRequest request) {
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = mapper.map(request, UserDto.class);
+        
+        userService.createUser(userDto);
+
+        return "Create user method is called";
     }
 }
---
+++ Spring/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.userservice.service;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.jpa.User;
+import com.example.userservice.jpa.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        userDto.setUserId(UUID.randomUUID().toString());
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        User user = mapper.map(userDto, User.class);
+        user.setEncryptedPwd("encrypted_password");
+
+        userRepository.save(user);
+        return null;
+    }
+}
