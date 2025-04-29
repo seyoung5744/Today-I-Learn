2cd70e0f82cb5adaff73a58fd31e44df9281e00d
seyoung5744 <seyoung574458@gmail.com>
2025-04-29 15:07:20 +0900

    [Spring boot 3 - Mail, OAuth2.0] - 3. InputBox 컴포넌트 구조 및 스타일 작성

---
+++ Spring/spring_boot-Mail_OAuth2.0/front/src/conponents/inputBox/index.tsx
@@ -0,0 +1,25 @@
+import { forwardRef } from 'react';
+import './style.css';
+
+interface Props {
+
+}
+
+const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
+
+
+    return (
+        <div className='input-box'>
+            <div className='input-box-title'>{'아이디'}</div>
+            <div className='input-box-content'>
+                <div className='input-box-body'>
+                    <input className='input-box-input' placeholder='아이디를 입력해주세요'/>
+                    <div className='input-box-button-disable'>{'중복 확인'}</div>
+                </div>
+                <div className='input-box-message-error'>{'사용 가능한 아이디입니다.'}</div>
+            </div>
+        </div>
+    )
+});
+
+export default InputBox;
