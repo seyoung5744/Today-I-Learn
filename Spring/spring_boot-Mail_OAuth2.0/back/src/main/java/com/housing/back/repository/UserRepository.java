aad7178020fde61b23baac01e71e74476c020968
seyoung5744 <seyoung574458@gmail.com>
2025-04-29 17:22:51 +0900

    [Spring boot 3 - Mail, OAuth2.0] - 7. JwtAuthenticationFilter 작성

--- Spring/spring_boot-Mail_OAuth2.0/back/src/main/java/com/housing/back/repository/UserRepository.java
+++ Spring/spring_boot-Mail_OAuth2.0/back/src/main/java/com/housing/back/repository/UserRepository.java
@@ -4,6 +4,10 @@ import com.housing.back.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
+
+    Optional<UserEntity> findByUserId(String userId);
 }
