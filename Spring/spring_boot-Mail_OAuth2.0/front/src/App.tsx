e96456ff1e3cc778f91a41dbd3af44e92b7c64a4
seyoung5744 <seyoung574458@gmail.com>
2025-04-29 15:46:02 +0900

    [Spring boot 3 - Mail, OAuth2.0] - 4. InputBox 컴포넌트 속성 및 공통 스타일 작성

--- Spring/spring_boot-Mail_OAuth2.0/front/src/App.tsx
+++ Spring/spring_boot-Mail_OAuth2.0/front/src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './App.css';
 import InputBox from 'conponents/inputBox';
 
 function App() {
+
+
+
   return (
     <>
-      <InputBox />
+      <div className='text-link-lg full-width'>회원가입</div>
+      <div className='kakao-sign-in-button'></div>
+      <div className='naver-sign-in-button'></div>
     </>
   );
 }
--- Spring/spring_boot-Mail_OAuth2.0/front/src/conponents/inputBox/index.tsx
+++ Spring/spring_boot-Mail_OAuth2.0/front/src/conponents/inputBox/index.tsx
@@ -1,22 +1,35 @@
-import { forwardRef } from 'react';
+import { ChangeEvent, KeyboardEvent, forwardRef } from 'react';
 import './style.css';
 
 interface Props {
-
+    title: string;
+    placeholder: string;
+    type: 'text' | 'password';
+    value: string;
+    isErrorMessage?: boolean;
+    buttonTitle?: string;
+    message?: string;
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    onKeyDown?: (event: KeyboardEvent<HTMLInputElement>) => void;
+    onButtonClick?: () => void;
 }
 
 const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
 
+    const {title, placeholder, type, value, isErrorMessage, buttonTitle, message, onChange, onKeyDown, onButtonClick} = props;
+
+    const buttonClass = value === '' ? 'input-box-button-disable' : 'input-box-button';
+    const messageClass = isErrorMessage ? 'input-box-message-error' : 'input-box-message';
 
     return (
         <div className='input-box'>
-            <div className='input-box-title'>{'아이디'}</div>
+            <div className='input-box-title'>{title}</div>
             <div className='input-box-content'>
                 <div className='input-box-body'>
-                    <input className='input-box-input' placeholder='아이디를 입력해주세요'/>
-                    <div className='input-box-button-disable'>{'중복 확인'}</div>
+                    <input ref={ref} className='input-box-input' placeholder={placeholder} type={type} value={value} onChange={onChange} onKeyDown={onKeyDown} />
+                    {buttonTitle !== undefined && onButtonClick !== undefined && <div className={buttonClass} onClick={onButtonClick}>{buttonTitle}</div>}
                 </div>
-                <div className='input-box-message-error'>{'사용 가능한 아이디입니다.'}</div>
+                {message !== undefined && <div className={messageClass}>{message}</div>}
             </div>
         </div>
     )
