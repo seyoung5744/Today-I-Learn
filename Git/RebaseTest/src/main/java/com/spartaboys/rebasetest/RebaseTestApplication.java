ed6dae6a2cdac90741f5b750cef71ff10283a688
seyoung5744 <seyoung574458@gmail.com>
2025-08-21 16:07:59 +0900

    create rebase test project

---
+++ Git/RebaseTest/src/main/java/com/spartaboys/rebasetest/RebaseTestApplication.java
@@ -0,0 +1,13 @@
+package com.spartaboys.rebasetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebaseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebaseTestApplication.class, args);
+	}
+
+}
---
+++ Git/RebaseTest/src/test/java/com/spartaboys/rebasetest/RebaseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartaboys.rebasetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebaseTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
