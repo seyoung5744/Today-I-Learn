bfd43287cb652250b06fe245e914919afb2e130e
seyoung5744 <seyoung574458@gmail.com>
2025-08-27 22:34:54 +0900

    스프링 AOP 구현 - 시작

--- Spring/05. 스프링 핵심 원리 - 고급편/aop/src/test/java/com/spartaboys/aop/AopTest.java
+++ Spring/05. 스프링 핵심 원리 - 고급편/aop/src/test/java/com/spartaboys/aop/AopTest.java
@@ -2,16 +2,19 @@ package com.spartaboys.aop;
 
 import com.spartaboys.aop.order.OrderRepository;
 import com.spartaboys.aop.order.OrderService;
+import com.spartaboys.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class)
 public class AopTest {
 
     @Autowired
