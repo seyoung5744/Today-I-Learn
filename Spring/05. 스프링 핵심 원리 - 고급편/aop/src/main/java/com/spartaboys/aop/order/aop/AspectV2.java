f3410695e7ab90ce935d0b8f3afa822cabfca4ae
seyoung5744 <seyoung574458@gmail.com>
2025-08-27 22:38:03 +0900

    포인트컷 분리

---
+++ Spring/05. 스프링 핵심 원리 - 고급편/aop/src/main/java/com/spartaboys/aop/order/aop/AspectV2.java
@@ -0,0 +1,22 @@
+package com.spartaboys.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class AspectV2 {
+
+    // com.spartaboys.aop 패키지와 하위 패키지
+    @Pointcut("execution(* com.spartaboys.aop.order..*(..))") // pointcut expression
+    private void allOrder() {} // pointcut signature
+
+    @Around("allOrder()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature()); // join point 시그니처
+        return joinPoint.proceed();
+    }
+}
--- Spring/05. 스프링 핵심 원리 - 고급편/aop/src/test/java/com/spartaboys/aop/AopTest.java
+++ Spring/05. 스프링 핵심 원리 - 고급편/aop/src/test/java/com/spartaboys/aop/AopTest.java
@@ -3,6 +3,7 @@ package com.spartaboys.aop;
 import com.spartaboys.aop.order.OrderRepository;
 import com.spartaboys.aop.order.OrderService;
 import com.spartaboys.aop.order.aop.AspectV1;
+import com.spartaboys.aop.order.aop.AspectV2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -14,7 +15,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
-@Import(AspectV1.class)
+//@Import(AspectV1.class)
+@Import(AspectV2.class)
 public class AopTest {
 
     @Autowired
