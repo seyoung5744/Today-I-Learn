5dd8a52a750645a587d851c783024faedf98c608
seyoung5744 <seyoung574458@gmail.com>
2025-08-28 17:48:42 +0900

    [13. 스프링 AOP - 실무 주의사항] 프록시와 내부 호출 - 문제

---
+++ Spring/05. 스프링 핵심 원리 - 고급편/aop/src/test/java/com/spartaboys/aop/internalcall/CallLogAspectTest.java
@@ -0,0 +1,24 @@
+package com.spartaboys.aop.internalcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(CallLogAspect.class)
+@SpringBootTest
+class CallLogAspectTest {
+
+    @Autowired
+    CallServiceV0 callServiceV0;
+
+    @Test
+    void external() {
+        callServiceV0.external();
+    }
+
+    @Test
+    void internal() {
+        callServiceV0.internal();
+    }
+}
