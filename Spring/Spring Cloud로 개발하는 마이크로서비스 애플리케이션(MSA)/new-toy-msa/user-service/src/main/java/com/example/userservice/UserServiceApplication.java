34f9e863f9e354dffda17c90e1e8009ecd85fa21
seyoung5744 <seyoung574458@gmail.com>
2025-09-07 00:43:03 +0900

    Users Microservice - 유저 서비스 구현

--- Spring/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/new-toy-msa/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ Spring/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/new-toy-msa/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,9 +1,11 @@
-package com.example;
+package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
---
+++ Spring/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/new-toy-msa/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
+import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
+import com.example.userservice.vo.ResponseUser;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class UserController {
+
+    private final Environment env;
+
+    private final Greeting greeting;
+
+    private final UserService userService;
+
+    @GetMapping("/health-check") // http://localhost:60000/health-check
+    public String status() {
+        return String.format("It's Working in User Service"
+                + ", port(local.server.port)=" + env.getProperty("local.server.port")
+                + ", port(server.port)=" + env.getProperty("server.port"));
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(HttpServletRequest request) {
+        log.info("users.welcome ip: {}, {}, {}, {}", request.getRemoteAddr()
+                , request.getRemoteHost(), request.getRequestURI(), request.getRequestURL());
+
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+    }
+}
