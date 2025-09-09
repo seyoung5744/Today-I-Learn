dbc89bf5e298b30d74afbdf05f2ac6c73075e925
seyoung5744 <seyoung574458@gmail.com>
2025-09-09 21:27:07 +0900

    Users Microservice - 사용자 조회

--- Spring/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/new-toy-msa/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ Spring/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/new-toy-msa/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -3,14 +3,18 @@ package com.example.userservice.service;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
+import com.example.userservice.vo.ResponseOrder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -38,5 +42,23 @@ public class UserServiceImpl implements UserService {
         UserDto returnUserDto = mapper.map(userEntity, UserDto.class);
 
         return returnUserDto;
+    }
+
+    @Override
+    public UserDto getUserByUserId(String userId) {
+        UserEntity userEntity = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("User not Found"));
+
+        UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
+
+        List<ResponseOrder> orders = new ArrayList<>();
+        userDto.setOrders(orders);
+
+        return userDto;
+    }
+
+    @Override
+    public List<UserEntity> getUserByAll() {
+        return userRepository.findAll();
     }
 }
