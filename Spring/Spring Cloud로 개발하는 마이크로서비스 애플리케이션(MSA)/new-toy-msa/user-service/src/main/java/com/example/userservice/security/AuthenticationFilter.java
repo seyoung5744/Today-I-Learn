8e7e33a2dfc8d5e846b7a07e964e41fc96428995
seyoung5744 <seyoung574458@gmail.com>
2025-09-11 22:36:21 +0900

    62. Users Microservice - 인증처리구현

---
+++ Spring/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/new-toy-msa/user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -0,0 +1,58 @@
+package com.example.userservice.security;
+
+import com.example.userservice.service.UserService;
+import com.example.userservice.vo.RequestLogin;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final UserService userService;
+    private final Environment env;
+
+    public AuthenticationFilter(UserService userService, Environment env, AuthenticationManager authenticationManager) {
+        this.userService = userService;
+        this.env = env;
+        super.setAuthenticationManager(authenticationManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+
+            RequestLogin creds = new ObjectMapper().readValue(request.getInputStream(), RequestLogin.class);
+
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getEmail(),
+                            creds.getPassword(),
+                            new ArrayList<>()
+                    )
+            );
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req,
+                                            HttpServletResponse res,
+                                            FilterChain chain,
+                                            Authentication authResult)
+            throws IOException, ServletException {
+       
+    }
+}
--- Spring/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/new-toy-msa/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ Spring/Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/new-toy-msa/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -5,10 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Configuration
 @EnableWebSecurity
@@ -17,21 +21,40 @@ public class WebSecurity {
 
     private final UserService userService;
     private final Environment env;
+    private final PasswordEncoder passwordEncoder;
 
 //    private static final String allowed_ip_address
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(passwordEncoder);
+
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
         http.csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/h2-console/**").permitAll() // 특정 경로 허용
+                        .requestMatchers("/**").access(
+                                new WebExpressionAuthorizationManager(
+                                        "hasIpAddress('127.0.0.1') or hasIpAddress('::1') or hasIpAddress('192.168.0.141') or hasIpAddress('::1')" // host pc ip address
+                                )
+                        )
                         .anyRequest().authenticated() // 그 외는 인증 필요
                 )
+                .authenticationManager(authenticationManager)
+                .addFilter(getAuthenticationFilter(authenticationManager))
                 .httpBasic(Customizer.withDefaults()) // Basic 인증 추가
                 .headers((headers) -> headers
                         .frameOptions((frameOptions) -> frameOptions.sameOrigin()));
 
         return http.build();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter(AuthenticationManager authenticationManager) {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(userService, env, authenticationManager);
+        authenticationFilter.setAuthenticationManager(authenticationManager);
+        return authenticationFilter;
     }
 
 }
