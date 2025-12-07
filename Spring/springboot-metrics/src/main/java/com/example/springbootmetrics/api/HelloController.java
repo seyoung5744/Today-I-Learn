4f47c1e80ae0044f6b9583e9e63cc9e66a6cde1b
seyoung5744 <seyoung574458@gmail.com>
2025-12-07 19:37:52 +0900

    프로메테우스 설정

--- Spring/springboot-metrics/src/main/java/com/example/springbootmetrics/api/HelloController.java
+++ Spring/springboot-metrics/src/main/java/com/example/springbootmetrics/api/HelloController.java
@@ -1,17 +1,26 @@
 package com.example.springbootmetrics.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class HelloController {
 
     private AtomicLong counter = new AtomicLong();
-    
-    @GetMapping("/hello")
-    public String world() {
+    private final Random random = new Random();
+
+    @GetMapping("/hello/{percent}")
+    public String world(@PathVariable Integer percent) throws InterruptedException {
+        if (random.nextInt(100) < percent) {
+            int sleepTime = 100 * random.nextInt(500);
+            TimeUnit.MICROSECONDS.sleep(sleepTime);
+            throw new RuntimeException("delayed");
+        }
         counter.incrementAndGet();
         return "world";
     }
