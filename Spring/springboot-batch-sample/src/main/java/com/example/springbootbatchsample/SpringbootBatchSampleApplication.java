4bb2032591e12723185dcb7e32e41d3b7bb985b7
seyoung5744 <seyoung574458@gmail.com>
2025-12-09 11:26:28 +0900

    create spring batch sample project

---
+++ Spring/springboot-batch-sample/src/main/java/com/example/springbootbatchsample/SpringbootBatchSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootbatchsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBatchSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBatchSampleApplication.class, args);
+    }
+
+}
---
+++ Spring/springboot-batch-sample/src/test/java/com/example/springbootbatchsample/SpringbootBatchSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootbatchsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBatchSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
