e7a95656eba6288b22c833c63b0e3c427a0927fe
seyoung5744 <seyoung574458@gmail.com>
2025-12-16 17:35:54 +0900

    create api first project

---
+++ Spring/springboot-api-first/src/main/java/com/example/springbootapifirst/SpringbootApiFirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootapifirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApiFirstApplication.class, args);
+    }
+
+}
---
+++ Spring/springboot-api-first/src/test/java/com/example/springbootapifirst/SpringbootApiFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootapifirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
