0bbcf1af17739795e3d573aa28023712036545e1
seyoung5744 <seyoung574458@gmail.com>
2025-12-20 17:15:06 +0900

    게시판 프로젝트 생성

---
+++ Spring/zerobaseboard/src/main/java/com/example/zerobaseboard/ZerobaseboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.zerobaseboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerobaseboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerobaseboardApplication.class, args);
+    }
+
+}
---
+++ Spring/zerobaseboard/src/test/java/com/example/zerobaseboard/ZerobaseboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zerobaseboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerobaseboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
