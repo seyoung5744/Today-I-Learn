cce1c0246ab8b8fdcf618e0ec3a81c23f1238d45
seyoung5744 <seyoung574458@gmail.com>
2025-12-24 14:56:47 +0900

    19. 게시글 목록 API - 무한 스크롤 구현

--- Spring/kuke-board/service/article/src/test/java/kuke/board/article/api/ArticleApiTest.java
+++ Spring/kuke-board/service/article/src/test/java/kuke/board/article/api/ArticleApiTest.java
@@ -5,7 +5,10 @@ import kuke.board.article.service.response.ArticleResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.client.RestClient;
+
+import java.util.List;
 
 public class ArticleApiTest {
 
@@ -73,6 +76,32 @@ public class ArticleApiTest {
             System.out.println("article = " + article);
         }
     }
+
+    @Test
+    void readAllInfiniteScrollTest() {
+        List<ArticleResponse> articles1 = restClient.get()
+                .uri("/v1/articles/infinite-scroll?boardId=1&pageSize=5")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<ArticleResponse>>() {
+                });
+        System.out.println("firstPage");
+        for (ArticleResponse response : articles1) {
+            System.out.println("response.getArticleId() = " + response.getArticleId());
+        }
+
+        Long lastArticleId = articles1.getLast().getArticleId();
+        List<ArticleResponse> articles2 = restClient.get()
+                .uri("/v1/articles/infinite-scroll?boardId=1&pageSize=5&lastArticleId=%s".formatted(lastArticleId))
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<ArticleResponse>>() {
+                });
+
+        System.out.println("secondPage");
+        for (ArticleResponse response : articles2) {
+            System.out.println("response.getArticleId() = " + response.getArticleId());
+        }
+    }
+
     @Getter
     @AllArgsConstructor
     static class ArticleCreateRequest {
