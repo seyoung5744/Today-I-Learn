982224d579100d3afd7224edd089eba6dc78ffee
seyoung5744 <seyoung574458@gmail.com>
2025-12-30 15:40:19 +0900

    45. 인기글 Consumer 구현 - 설정 및 인기글 레포지토리

---
+++ Spring/kuke-board/service/hot-article/src/main/java/kuke/board/hotarticle/config/KafkaConfig.java
@@ -0,0 +1,21 @@
+package kuke.board.hotarticle.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
+            ConsumerFactory<String, String> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL); // auto-commit 을 false 로 해뒀기 때문에 필요한 설정
+        return factory;
+    }
+}
