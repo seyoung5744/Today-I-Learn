62a39caa975359cd7c1a0ab8e13f1dfc591c0237
seyoung5744 <seyoung574458@gmail.com>
2026-01-04 17:23:35 +0900

    58. 게시글 조회 최적화 구현 - 조회수 캐시

--- Spring/kuke-board/service/article-read/src/main/java/kuke/board/articleread/client/ViewClient.java
+++ Spring/kuke-board/service/article-read/src/main/java/kuke/board/articleread/client/ViewClient.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -22,7 +23,12 @@ public class ViewClient {
         restClient = RestClient.create(viewServiceUrl);
     }
 
+    // 레디스에서 데이터 조회
+    // 레디스에 데이터가 없다면, count 메서드가 호출되면서 viewService로 원본 데이터 요청. 그리고 레디스에 데이터를 저장하고 응답
+    // 레디스에 데이터가 있었다면, 해당 데이터를 그대로 바로 응답
+    @Cacheable(key = "#articleId", value = "articleViewCount")
     public Long count(Long articleId) {
+        log.info("[ViewClient.count] articleId={}", articleId);
         try {
             return restClient.get()
                     .uri("/v1/article-views/articles/{articleId}/count", articleId)
