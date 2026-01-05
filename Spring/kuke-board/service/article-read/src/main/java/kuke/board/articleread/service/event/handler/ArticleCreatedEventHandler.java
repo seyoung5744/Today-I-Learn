2c42b244ff645c9362b65c3f30fa5b0dc781b0e6
seyoung5744 <seyoung574458@gmail.com>
2026-01-05 18:45:42 +0900

    64. 게시글 조회 최적화 전략 구현 - 서비스 & 컨트롤러

--- Spring/kuke-board/service/article-read/src/main/java/kuke/board/articleread/service/event/handler/ArticleCreatedEventHandler.java
+++ Spring/kuke-board/service/article-read/src/main/java/kuke/board/articleread/service/event/handler/ArticleCreatedEventHandler.java
@@ -1,7 +1,9 @@
 package kuke.board.articleread.service.event.handler;
 
+import kuke.board.articleread.repository.ArticleIdListRepository;
 import kuke.board.articleread.repository.ArticleQueryModel;
 import kuke.board.articleread.repository.ArticleQueryModelRepository;
+import kuke.board.articleread.repository.BoardArticleCountRepository;
 import kuke.board.common.event.Event;
 import kuke.board.common.event.EventType;
 import kuke.board.common.event.payload.ArticleCreatedEventPayload;
@@ -15,14 +17,31 @@ import java.time.Duration;
 public class ArticleCreatedEventHandler implements EventHandler<ArticleCreatedEventPayload> {
 
     private final ArticleQueryModelRepository articleQueryModelRepository;
+    private final ArticleIdListRepository articleIdListRepository;
+    private final BoardArticleCountRepository boardArticleCountRepository;
 
     @Override
     public void handle(Event<ArticleCreatedEventPayload> event) {
         ArticleCreatedEventPayload payload = event.getPayload();
+        /*
+            처리 순서 중요!!
+
+            articleQueryModelRepository를 먼저 호출해야 게시글 목록에 노출되었을 때 실제 게시글 내용이
+            정상적으로 조회 가능한 상태가 된다.
+
+            만약 articleIdListRepository를 먼저 호출하면, 게시글 ID는 목록에 존재하지만
+
+            아직 게시글 본문이 생성되지 않아 불완전한 조회 상태가 발생할 수 있다.
+
+            따라서 게시글 내용(조회 모델)을 먼저 생성한 뒤, 게시글 ID를 목록에 추가하여
+            목록과 내용의 일관성을 보장한다.
+         */
         articleQueryModelRepository.create(
                 ArticleQueryModel.create(payload),
                 Duration.ofDays(1)
         );
+        articleIdListRepository.add(payload.getBoardId(), payload.getArticleId(), 1000L);
+        boardArticleCountRepository.createOrUpdate(payload.getBoardId(), payload.getBoardArticleCount());
     }
 
     @Override
