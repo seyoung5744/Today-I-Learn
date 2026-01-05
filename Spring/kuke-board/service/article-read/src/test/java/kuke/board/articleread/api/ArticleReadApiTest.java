1fecabb48fd1b12398e6db650e00f39f422b32d2
seyoung5744 <seyoung574458@gmail.com>
2026-01-05 19:10:45 +0900

    65. 게시글 조회 최적화 전략 구현 - 테스트

--- Spring/kuke-board/service/article-read/src/test/java/kuke/board/articleread/api/ArticleReadApiTest.java
+++ Spring/kuke-board/service/article-read/src/test/java/kuke/board/articleread/api/ArticleReadApiTest.java
@@ -1,20 +1,75 @@
 package kuke.board.articleread.api;
 
+import kuke.board.articleread.service.response.ArticleReadPageResponse;
 import kuke.board.articleread.service.response.ArticleReadResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.client.RestClient;
+
+import java.util.List;
 
 public class ArticleReadApiTest {
 
-    RestClient restClient = RestClient.create("http://localhost:8005");
+    RestClient articleReadRestClient = RestClient.create("http://localhost:8005");
+
+    // 원본 데이터와의 비교를 위한 RestClient
+    RestClient articleRestClient = RestClient.create("http://localhost:8000");
 
     @Test
     void readTest() {
         // given
-        ArticleReadResponse response = restClient.get()
+        ArticleReadResponse response = articleReadRestClient.get()
                 .uri("/v1/articles/{articleId}", 266154838979391488L)
                 .retrieve()
                 .body(ArticleReadResponse.class);
         System.out.println("response = " + response);
+    }
+
+
+    @Test
+    void readAllTest() {
+        ArticleReadPageResponse response1 = articleReadRestClient.get()
+                .uri("/v1/articles?boardId=%s&page=%s&pageSize=%s".formatted(1L, 20L, 5L))
+                .retrieve()
+                .body(ArticleReadPageResponse.class);
+
+        System.out.println("response1.getArticleCount() = " + response1.getArticleCount());
+        for (ArticleReadResponse article : response1.getArticles()) {
+            System.out.println("article.getArticleId() = " + article.getArticleId());
+        }
+
+        ArticleReadPageResponse response2 = articleRestClient.get()
+                .uri("/v1/articles?boardId=%s&page=%s&pageSize=%s".formatted(1L, 20L, 5L))
+                .retrieve()
+                .body(ArticleReadPageResponse.class);
+
+        System.out.println("response2.getArticleCount() = " + response2.getArticleCount());
+        for (ArticleReadResponse article : response2.getArticles()) {
+            System.out.println("article.getArticleId() = " + article.getArticleId());
+        }
+    }
+
+    @Test
+    void readAllInfiniteScrollTest() {
+        List<ArticleReadResponse> responses1 = articleReadRestClient.get()
+                .uri("/v1/articles/infinite-scroll?boardId=%s&pageSize=%s&lastArticleId=%s".formatted(1L, 5L, 266506898960474112L))
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<ArticleReadResponse>>() {
+                });
+
+        for (ArticleReadResponse response : responses1) {
+            System.out.println("response = " + response.getArticleId());
+        }
+
+        List<ArticleReadResponse> responses2 = articleRestClient.get()
+                .uri("/v1/articles/infinite-scroll?boardId=%s&pageSize=%s&lastArticleId=%s".formatted(1L, 5L, 266506898960474112L))
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<ArticleReadResponse>>() {
+                });
+
+        for (ArticleReadResponse response : responses2) {
+            System.out.println("response = " + response.getArticleId());
+        }
+
     }
 }
