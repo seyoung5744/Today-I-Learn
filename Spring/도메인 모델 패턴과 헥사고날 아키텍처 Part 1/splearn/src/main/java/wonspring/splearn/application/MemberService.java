81db7ab6de3a0262c26869462a8a485a6d2f6576
seyoung5744 <seyoung574458@gmail.com>
2026-02-16 14:38:14 +0900

    26. 회원 애플리케이션 서비스 구현

---
+++ Spring/도메인 모델 패턴과 헥사고날 아키텍처 Part 1/splearn/src/main/java/wonspring/splearn/application/MemberService.java
@@ -0,0 +1,34 @@
+package wonspring.splearn.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import wonspring.splearn.application.provided.MemberRegister;
+import wonspring.splearn.application.required.EmailSender;
+import wonspring.splearn.application.required.MemberRepository;
+import wonspring.splearn.domain.Member;
+import wonspring.splearn.domain.MemberRegisterRequest;
+import wonspring.splearn.domain.PasswordEncoder;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService implements MemberRegister {
+
+    private final MemberRepository memberRepository;
+    private final EmailSender emailSender;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Member register(MemberRegisterRequest registerRequest) {
+        // check
+        // domain model
+        Member member = Member.register(registerRequest, passwordEncoder);
+
+        // repository
+        memberRepository.save(member);
+
+        // post process
+        emailSender.send(member.getEmail(), "등록을 완료해주세요", "아래 링크를 클릭해서 등록을 완료해주세요.");
+
+        return member;
+    }
+}
