a94c80b954de2725d696b9423adb1714a28b1813
seyoung5744 <seyoung574458@gmail.com>
2026-02-20 17:07:05 +0900

    41. MemberApi와 웹 단위 테스트

---
+++ Spring/domain-model-pattern-and-hexagonal-architecture-part-1/splearn/src/main/java/wonspring/splearn/adapter/webapi/MemberApi.java
@@ -0,0 +1,27 @@
+package wonspring.splearn.adapter.webapi;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import wonspring.splearn.adapter.webapi.dto.MemberRegisterResponse;
+import wonspring.splearn.application.member.provided.MemberRegister;
+import wonspring.splearn.domain.member.Member;
+import wonspring.splearn.domain.member.MemberRegisterRequest;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApi {
+
+    private final MemberRegister memberRegister;
+
+    // register api -> /members POST
+    @PostMapping("/api/members")
+    public MemberRegisterResponse register(@RequestBody @Valid MemberRegisterRequest request) {
+        Member member = memberRegister.register(request);
+
+        return MemberRegisterResponse.of(member);
+    }
+
+}
---
+++ Spring/domain-model-pattern-and-hexagonal-architecture-part-1/splearn/src/test/java/wonspring/splearn/adapter/webapi/MemberApiTest.java
@@ -0,0 +1,68 @@
+package wonspring.splearn.adapter.webapi;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import wonspring.splearn.application.member.provided.MemberRegister;
+import wonspring.splearn.domain.member.Member;
+import wonspring.splearn.domain.member.MemberFixture;
+import wonspring.splearn.domain.member.MemberRegisterRequest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@WebMvcTest(MemberApi.class)
+@RequiredArgsConstructor
+class MemberApiTest {
+
+    final MockMvcTester mvcTester;
+    final ObjectMapper objectMapper;
+
+    @MockitoBean
+    MemberRegister memberRegister;
+
+    @Test
+    void register() throws JsonProcessingException {
+        Member member = MemberFixture.createMember(1L);
+        given(memberRegister.register(any())).willReturn(member);
+
+        MemberRegisterRequest request = MemberFixture.createMemberRegisterRequest();
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        assertThat(
+                mvcTester.post()
+                        .uri("/api/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson)
+        )
+                .hasStatusOk()
+                .bodyJson()
+                .extractingPath("$.memberId").asNumber().isEqualTo(1);
+
+        verify(memberRegister).register(request);
+    }
+
+    @Test
+    void registerFail() throws JsonProcessingException {
+
+        MemberRegisterRequest request = MemberFixture.createMemberRegisterRequest("Invalid Email");
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        assertThat(
+                mvcTester.post()
+                        .uri("/api/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson)
+        )
+                .hasStatus(HttpStatus.BAD_REQUEST);
+    }
+    
+}
