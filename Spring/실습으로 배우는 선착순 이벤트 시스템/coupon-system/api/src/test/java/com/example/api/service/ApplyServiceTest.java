77078368800603c8b203828a43b246001b8e506f
seyoung5744 <seyoung574458@gmail.com>
2026-03-15 16:57:49 +0900

    6. 쿠폰 발급 동시성 이슈

--- Spring/실습으로 배우는 선착순 이벤트 시스템/coupon-system/api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ Spring/실습으로 배우는 선착순 이벤트 시스템/coupon-system/api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -23,5 +27,28 @@ class ApplyServiceTest {
         long count = couponRepository.count();
 
         assertThat(count).isEqualTo(1L);
+    }
+
+    @Test
+    void 여러번응모() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        long count = couponRepository.count();
+        assertThat(count).isEqualTo(100);
     }
 }
